03aab545c6d60bf4f201f579419e96e96ac9a1bc
wenyongh <wenyong.huang@intel.com>
2019-09-06 16:28:34 +0800

    Update wasm_application.h
    Add comments for the functions.

--- core/iwasm/runtime/include/wasm_application.h
+++ core/iwasm/runtime/include/wasm_application.h
@@ -26,10 +26,33 @@ extern "C" {
 
 struct WASMModuleInstance;
 
+/**
+ * Find the unique main function from a WASM module instance
+ * and execute that function.
+ *
+ * @param module_inst the WASM module instance
+ * @param argc the number of arguments
+ * @param argv the arguments array
+ *
+ * @return true if the main function is called, false otherwise and exception will be thrown,
+ *   the caller can call wasm_runtime_get_exception to get exception info.
+ */
 bool
 wasm_application_execute_main(struct WASMModuleInstance *module_inst,
                               int argc, char *argv[]);
 
+/**
+ * Find the specified function in argv[0] from a WASM module instance
+ * and execute that function.
+ *
+ * @param module_inst the WASM module instance
+ * @param name the name of the function to execute
+ * @param argc the number of arguments
+ * @param argv the arguments array
+ *
+ * @return true if the specified function is called, false otherwise and exception will be thrown,
+ *   the caller can call wasm_runtime_get_exception to get exception info.
+ */
 bool
 wasm_application_execute_func(struct WASMModuleInstance *module_inst,
                               char *name, int argc, char *argv[]);
