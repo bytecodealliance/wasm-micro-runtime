4fcb7d45cad5aa518d00f481571fcf0fdd781de7
Istvan Haller <ihaller@microsoft.com>
2020-07-30 17:04:01 +0100

    Removed need to change snmalloc, moved PAL implementation to wrapper

--- core/shared/mem-alloc/mem_alloc_snmalloc.cc
+++ core/shared/mem-alloc/mem_alloc_snmalloc.cc
@@ -12,6 +12,73 @@
 #define SNMALLOC_NAME_MANGLE(a) wamr_sn_##a
 // Redefine the namespace, so we can a different allocator from the system one.
 #define snmalloc snmalloc_wamr
+
+#include "../../../snmalloc/src/ds/address.h"
+#include "../../../snmalloc/src/ds/flaglock.h"
+#include <array>
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace snmalloc
+{
+  class PALFixedRegion
+  {
+    /// Base of OE heap
+    static inline void* heap_base = nullptr;
+
+    /// Size of OE heap
+    static inline size_t heap_size;
+
+    // This is infrequently used code, a spin lock simplifies the code
+    // considerably, and should never be on the fast path.
+    static inline std::atomic_flag spin_lock;
+
+  public:
+    /**
+     * This will be called by oe_allocator_init to set up enclave heap bounds.
+     */
+    static void setup_initial_range(void* base, void* end)
+    {
+      heap_size = pointer_diff(base, end);
+      heap_base = base;
+    }
+
+    /**
+     * Bitmap of PalFeatures flags indicating the optional features that this
+     * PAL supports.
+     */
+    static constexpr uint64_t pal_features = 0;
+
+    static constexpr size_t page_size = 0x1000;
+
+    [[noreturn]] static void error(const char* const str)
+    {
+      fprintf(stderr, "%s\n", str);
+      abort();
+    }
+
+    static std::pair<void*, size_t>
+    reserve_at_least(size_t request_size) noexcept
+    {
+      // First call returns the entire address space
+      // subsequent calls return {nullptr, 0}
+      FlagLock lock(spin_lock);
+      if (request_size > heap_size)
+        return {nullptr, 0};
+
+      auto result = std::make_pair(heap_base, heap_size);
+      heap_size = 0;
+      return result;
+    }
+
+    template<bool page_aligned = false>
+    void zero(void* p, size_t size) noexcept
+    {
+      memset(p, size, 0);
+    }
+  };
+}
+
 #include "../../../snmalloc/src/override/malloc.cc"
 
 extern "C" {
