1266ebb2227e5fff5aece90a7dcae081766df693
Xu Jun <693788454@qq.com>
2020-08-13 16:40:19 +0800

    fix coding style for windows build patch (#350)

--- core/shared/platform/windows/win_time.c
+++ core/shared/platform/windows/win_time.c
@@ -11,6 +11,6 @@ os_time_get_boot_microsecond()
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
 
-    return ((uint64) ts.tv_sec) * 1000 * 1000 + ((uint64)ts.tv_nsec) / 1000;
+    return ((uint64)ts.tv_sec) * 1000 * 1000 + ((uint64)ts.tv_nsec) / 1000;
 }
 
