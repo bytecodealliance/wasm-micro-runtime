0d8ce00fb394548be7d65c9f4129cc64f8f97916
Wenyong Huang <wenyong.huang@intel.com>
2020-12-04 15:24:50 +0800

    Fix app manager issues and optimize workload scripts

--- core/app-framework/base/app/bh_platform.h
+++ core/app-framework/base/app/bh_platform.h
@@ -45,4 +45,18 @@ uint16 ntohs(uint16 value);
 // We are not worried for the WASM world since the sandbox will catch it.
 #define bh_memcpy_s(dst, dst_len, src, src_len)  memcpy(dst, src, src_len)
 
+#ifdef NDEBUG
+#define bh_assert(v) (void)0
+#else
+#define bh_assert(v) do {                             \
+    if (!(v)) {                                       \
+        int _count;                                   \
+        printf("ASSERTION FAILED: %s, at %s, line %d",\
+               #v, __FILE__, __LINE__);               \
+        _count = printf("\n");                        \
+        printf("%d\n", _count / (_count - 1));        \
+    }                                                 \
+  } while (0)
+#endif
+
 #endif /* DEPS_IWASM_APP_LIBS_BASE_BH_PLATFORM_H_ */
