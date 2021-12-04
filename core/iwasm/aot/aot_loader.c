02d775fe187a87a10c5251d19ee33fd3fec66783
Wenyong Huang <wenyong.huang@intel.com>
2021-12-05 04:16:47 +0800

    Minor change

--- core/iwasm/aot/aot_loader.c
+++ core/iwasm/aot/aot_loader.c
@@ -2191,7 +2191,7 @@ load_relocation_section(const uint8 *buf, const uint8 *buf_end,
 #endif
         ) {
 #if !defined(BH_PLATFORM_LINUX) && !defined(BH_PLATFORM_LINUX_SGX) \
-    && !defined(BH_PLATFORM_DARWIN) && !defined(BH_PLATFORM_WINDOWS)
+    && !defined(BH_PLATFORM_DARWIN)
             if (module->native_symbol_count > 0) {
                 set_error_buf(error_buf, error_buf_size,
                               "cannot apply relocation to text section "
