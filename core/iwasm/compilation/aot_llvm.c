187a04450631a75446677d12bd451dff1cc2e5eb
Huang Qi <huangqi3@xiaomi.com>
2021-12-19 11:36:07 +0800

    Cleanup output format warning
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- core/iwasm/compilation/aot_llvm.c
+++ core/iwasm/compilation/aot_llvm.c
@@ -2490,7 +2490,7 @@ aot_load_const_from_table(AOTCompContext *comp_ctx, LLVMValueRef base,
     switch (value_type) {
         case VALUE_TYPE_F32:
             /* Store the raw int bits of f32 const as a hex string */
-            snprintf(buf, sizeof(buf), "f32#%08X", value->i32);
+            snprintf(buf, sizeof(buf), "f32#%08" PRIx32, value->i32);
             const_ptr_type = F32_PTR_TYPE;
             break;
         case VALUE_TYPE_F64:
