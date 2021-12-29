60facc172be16adadd68835be08739a1c8f53cea
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2021-12-29 17:00:22 +0100

    esp-idf: Implement os_thread_get_stack_boundary
    The stack base address is only available when tracing is on. Thus add
    an sdkconfig that has tracing on.

--- product-mini/platforms/esp-idf/build.sh
+++ product-mini/platforms/esp-idf/build.sh
@@ -1,4 +1,7 @@
+#!/bin/bash -e
+
 rm -rf build && mkdir build && cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32c3.cmake -DIDF_TARGET=esp32c3 -DCMAKE_BUILD_TYPE=Release -GNinja
+cp ../sdkconfig .
 cmake --build .
 ninja flash
