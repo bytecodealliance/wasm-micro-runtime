19b84a7447bbc205aeb558ff47e089d78012e86e
liang.he@intel.com <liang.he@intel.com>
2022-01-04 13:06:06 +0800

    Update doc of multi-module
    - update dockerfile of ci
    
    [skip ci]

--- ci/build_wamr.sh
+++ ci/build_wamr.sh
@@ -8,26 +8,28 @@ readonly ROOT=$(realpath "${CURRENT_PATH}/..")
 readonly VARIANT=$(lsb_release -c | awk '{print $2}')
 
 docker build \
-  --build-arg VARIANT=${VARIANT} \
-  --memory=4G --cpu-quota=50000 \
-  -t wamr_dev_${VARIANT}:0.1 -f "${CURRENT_PATH}"/Dockerfile "${CURRENT_PATH}" &&
-  docker run --rm -it \
-    --cpus=".5" \
-    --memory=4G \
-    --name wamr_build_env \
-    --mount type=bind,src="${ROOT}",dst=/workspace \
-    wamr_dev_${VARIANT}:0.1 \
-    /bin/bash -c "\
-      pwd \
-      && pushd product-mini/platforms/linux \
-      && rm -rf build \
-      && mkdir build  \
-      && pushd build \
-      && cmake .. \
-      && make \
-      && popd \
-      && popd \
-      && echo 'Copying the binary ...' \
-      && rm -rf build_out \
-      && mkdir build_out \
-      && cp product-mini/platforms/linux/build/iwasm build_out/iwasm"
+    --build-arg VARIANT=${VARIANT} \
+    --memory=4G --cpu-quota=50000 \
+    -t wamr_dev_${VARIANT}:0.1 -f "${CURRENT_PATH}"/Dockerfile "${CURRENT_PATH}" \
+  && docker run --rm -it \
+      --cap-add=SYS_PTRACE \
+      --cpus=".5" \
+      --memory=4G \
+      --mount type=bind,src="${ROOT}",dst=/workspace \
+      --name wamr_build_env \
+      --security-opt=seccomp=unconfined \
+      wamr_dev_${VARIANT}:0.1 \
+      /bin/bash -c "\
+        pwd \
+        && pushd product-mini/platforms/linux \
+        && rm -rf build \
+        && mkdir build  \
+        && pushd build \
+        && cmake .. \
+        && make \
+        && popd \
+        && popd \
+        && echo 'Copying the binary ...' \
+        && rm -rf build_out \
+        && mkdir build_out \
+        && cp product-mini/platforms/linux/build/iwasm build_out/iwasm"
