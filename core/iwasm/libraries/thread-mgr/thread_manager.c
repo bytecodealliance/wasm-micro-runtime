866fe8b0be1f8b5892102bcd44264906b6dea6ed
HangedFish <bravohangedman@outlook.com>
2022-03-21 14:07:53 +0800

    added_wasi_ctx_when_spawn_exec_env

--- core/iwasm/libraries/thread-mgr/thread_manager.c
+++ core/iwasm/libraries/thread-mgr/thread_manager.c
@@ -393,6 +393,11 @@ wasm_cluster_spawn_exec_env(WASMExecEnv *exec_env)
     wasm_runtime_set_custom_data_internal(
         new_module_inst, wasm_runtime_get_custom_data(module_inst));
 
+#if WASM_ENABLE_LIBC_WASI != 0
+    WASIContext* wasi_ctx = wasm_runtime_get_wasi_ctx(module_inst);
+    wasm_runtime_set_wasi_ctx(new_module_inst, wasi_ctx);
+#endif
+
     new_exec_env = wasm_exec_env_create_internal(new_module_inst,
                                                  exec_env->wasm_stack_size);
     if (!new_exec_env)
