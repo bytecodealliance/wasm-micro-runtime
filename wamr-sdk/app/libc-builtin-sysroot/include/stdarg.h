d6991d95deecb538cdb2479bd8e040523455c175
Xu Jun <693788454@qq.com>
2022-04-24 00:19:28 +0800

    clang auto format

--- wamr-sdk/app/libc-builtin-sysroot/include/stdarg.h
+++ wamr-sdk/app/libc-builtin-sysroot/include/stdarg.h
@@ -13,10 +13,10 @@ typedef __builtin_va_list va_list;
 #define _VA_LIST
 #endif
 #define va_start(ap, param) __builtin_va_start(ap, param)
-#define va_end(ap)          __builtin_va_end(ap)
-#define va_arg(ap, type)    __builtin_va_arg(ap, type)
+#define va_end(ap) __builtin_va_end(ap)
+#define va_arg(ap, type) __builtin_va_arg(ap, type)
 
-#define __va_copy(d,s) __builtin_va_copy(d,s)
+#define __va_copy(d, s) __builtin_va_copy(d, s)
 
 #endif /* __cplusplus */
 
