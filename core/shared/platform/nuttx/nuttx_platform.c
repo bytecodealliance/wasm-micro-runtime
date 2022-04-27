a7c6d08c3d4bbe148c765a22da747bdc5d6d73e5
YAMAMOTO Takashi <yamamoto@midokura.com>
2022-04-27 10:41:18 +0900

    core/shared/platform/nuttx: mock several APIs for libc-wasi

--- core/shared/platform/nuttx/nuttx_platform.c
+++ core/shared/platform/nuttx/nuttx_platform.c
@@ -57,3 +57,83 @@ os_mprotect(void *addr, size_t size, int prot)
 void
 os_dcache_flush()
 {}
+
+int
+openat(int fd, const char *path, int oflags, ...)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+int
+fstatat(int fd, const char *path, struct stat *buf, int flag)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+int
+mkdirat(int fd, const char *path, mode_t mode)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+ssize_t
+readlinkat(int fd, const char *path, char *buf, size_t bufsize)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+int
+linkat(int fd1, const char *path1, int fd2, const char *path2, int flag)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+int
+renameat(int fromfd, const char *from, int tofd, const char *to)
+{
+    errno = ENOSYS;
+    return -1;
+}
+int
+symlinkat(const char *target, int fd, const char *path)
+{
+    errno = ENOSYS;
+    return -1;
+}
+int
+unlinkat(int fd, const char *path, int flag)
+{
+    errno = ENOSYS;
+    return -1;
+}
+int
+utimensat(int fd, const char *path, const struct timespec ts[2], int flag)
+{
+    errno = ENOSYS;
+    return -1;
+}
+
+DIR *
+fdopendir(int fd)
+{
+    errno = ENOSYS;
+    return NULL;
+}
+
+ssize_t
+preadv(int fd, const struct iovec *iov, int iovcnt, off_t off)
+{
+    errno = ENOSYS;
+    return -1;
+}
+ssize_t
+pwritev(int fd, const struct iovec *iov, int iovcnt, off_t off)
+{
+    errno = ENOSYS;
+    return -1;
+}
