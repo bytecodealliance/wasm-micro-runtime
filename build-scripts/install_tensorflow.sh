0c5442d862b8d05b7cb95ac97edc24916335b48a
tonibofarull0 <toni.bofarull@midokura.com>
2022-06-08 17:41:20 +0200

    First initial design of wasi nn

---
+++ build-scripts/install_tensorflow.sh
@@ -0,0 +1,1 @@
+echo "IM INSIDE BUILD-SCRIPTS"
--- build-scripts/runtime_lib.cmake
+++ build-scripts/runtime_lib.cmake
@@ -91,6 +91,13 @@ if (WAMR_BUILD_LIB_PTHREAD_SEMAPHORE EQUAL 1)
     set (WAMR_BUILD_LIB_PTHREAD 1)
 endif ()
 
+if (WAMR_BUILD_WASI_NN EQUAL 1)
+    execute_process(COMMAND ${WAMR_ROOT_DIR}/build-scripts/install_tensorflow.sh
+                    RESULT_VARIABLE TENSORFLOW_RESULT
+    )
+    include (${IWASM_DIR}/libraries/wasi-nn/wasi_nn.cmake)
+endif ()
+
 if (WAMR_BUILD_LIB_PTHREAD EQUAL 1)
     include (${IWASM_DIR}/libraries/lib-pthread/lib_pthread.cmake)
     # Enable the dependent feature if lib pthread is enabled
