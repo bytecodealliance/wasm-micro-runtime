45a2de831b081e59c95d014d5b1348df356b2d88
Ahmedounet <ahmed.zouaoui.555@gmail.com>
2022-06-10 18:22:02 +0200

    feat: partial witx api of build and load

--- core/iwasm/libraries/wasi-nn/wasi_nn.h
+++ core/iwasm/libraries/wasi-nn/wasi_nn.h
@@ -9,3 +9,28 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef uint32_t buffer_size;
+
+typedef enum:uint16_t {sucess, invalid_argument, missing_memory, busy } nn_erno; 
+
+typedef uint32_t *  tensor_dimensions;
+
+typedef enum:uint8_t {f16, f32, u8, i32};
+
+typedef struct {
+
+tensor_dimensions dimensions;
+
+tensor_type type;
+
+tensor_data data;
+
+} tensor ;
+
+typedef uint8_t * graph_builder;
+
+typedef graph_builder * graph_builder_array;
+
+typedef handle graph;
+
+typedef enum {openvino=0, tensorflow,onnx } graph_encoding;
