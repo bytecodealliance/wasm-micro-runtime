cd4579aedbca38d25bb5f53f471797b87541cc6e
Wang Ning <ning4.wang@intel.com>
2022-07-19 23:15:50 +0800

    WAMR-IDE:   - update wasm-toolchain dockerfile and resource

--- test-tools/wamr-ide/WASM-Toolchain/Docker/resource/build_wasm.sh
+++ test-tools/wamr-ide/WASM-Toolchain/Docker/resource/build_wasm.sh
@@ -5,28 +5,21 @@
 export CC=/opt/wasi-sdk/bin/clang
 export CXX=/opt/wasi-sdk/bin/clang++
 
-HOSTOS=$2
-
-if [ ${HOSTOS} == "win32" ];then
-  WORKDIR="/mnt"
-elif [ ${HOSTOS} == "linux" ];then
-  WORKDIR=$(pwd)
+cd /mnt
+if [ -d build ];then
+  rm -fr build
 fi
 
-if [ -d ${WORKDIR}/build ];then
-  rm -fr ${WORKDIR}/build
-fi
-
-mkdir -p ${WORKDIR}/build
-cd ${WORKDIR}/build/
+mkdir -p build && cd build
 echo "========> compile wasm with wasi-sdk"
 cmake -DWASI_SDK_DIR=/opt/wasi-sdk -DCMAKE_TOOLCHAIN_FILE=/opt/wamr-sdk/app/wamr_toolchain.cmake ../.wamr && make
 
-echo && echo
-echo "========> compile wasm to AoT with wamrc"
-# target name will be provided:
-#    - user input the target name in IDE
-#    - generated wasm binary name will be set as user's input target name
-#    - aot binary name should be the same as wasm binary name
-#    - target name will be provided through 1st parameter
-wamrc -o $1.aot $1.wasm
+if [ $? -eq 0 ]; then
+  echo "========> compile wasm to AoT with wamrc"
+  # target name will be provided:
+  #    - user input the target name in IDE
+  #    - generated wasm binary name will be set as user's input target name
+  #    - aot binary name should be the same as wasm binary name
+  #    - target name will be provided through 1st parameter
+  wamrc -o $1.aot $1.wasm
+fi
