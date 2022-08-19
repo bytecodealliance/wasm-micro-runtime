cf45ba46424f4caa7e165f20795d47f5ea1e6e4c
FromLiQg <1501077298@qq.com>
2022-08-19 11:07:31 +0800

    Delete some comment unused, fix a simple bug in wasm_mini_loader.c.

--- core/iwasm/interpreter/wasm_mini_loader.c
+++ core/iwasm/interpreter/wasm_mini_loader.c
@@ -411,7 +411,7 @@ load_type_section(const uint8 *buf, const uint8 *buf_end, WASMModule *module,
 
             /* If there is already a same type created, use it instead */
             for (j = 0; j < i; ++j) {
-                if (wasm_type_equal(type, j)) {
+                if (wasm_type_equal(type, module->types[j])) {
                     bh_assert(module->types[j]->ref_count != UINT16_MAX);
                     destroy_wasm_type(type);
                     module->types[i] = module->types[j];
