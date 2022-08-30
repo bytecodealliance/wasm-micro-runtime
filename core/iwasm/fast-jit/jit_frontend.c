4b1ff8fc6d55720d0e6b01d5496dc7e39cebda7c
FromLiQg <1501077298@qq.com>
2022-08-30 19:16:06 +0800

    Fix bug: No error was detected and returned after the instruction generation failed.

--- core/iwasm/fast-jit/jit_frontend.c
+++ core/iwasm/fast-jit/jit_frontend.c
@@ -707,6 +707,9 @@ form_and_translate_func(JitCompContext *cc)
                 JitReg module_inst_reg = jit_cc_new_reg_ptr(cc);
                 GEN_INSN(LDPTR, module_inst_reg, cc->exec_env_reg,
                          NEW_CONST(I32, offsetof(WASMExecEnv, module_inst)));
+                if (jit_get_last_error(cc)){
+                    return false;
+                }
                 insn = GEN_INSN(
                     CALLNATIVE, 0,
                     NEW_CONST(PTR, (uintptr_t)jit_set_exception_with_id), 2);
