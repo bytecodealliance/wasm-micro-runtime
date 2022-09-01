3d31c182b48a5bf4ba468cd3dd7d92017630e8ad
Zeuson0 <511939992@qq.com>
2022-09-01 18:00:38 +0800

    Enable remote attestation by librats in SGX mode

--- core/config.h
+++ core/config.h
@@ -135,6 +135,10 @@
 #define WASM_ENABLE_LIBC_EMCC 0
 #endif
 
+#ifndef WASM_ENABLE_LIB_RATS
+#define WASM_ENABLE_LIB_RATS 0
+#endif
+
 #ifndef WASM_ENABLE_LIB_PTHREAD
 #define WASM_ENABLE_LIB_PTHREAD 0
 #endif
