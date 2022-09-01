e24cbfa4f47cc8697bc44aee4f7b53b630450e9a
Chenyu Yang <chenyu.yang@intel.com>
2022-09-02 02:24:41 +0800

    Modified the format according to Wenyong's comments

--- core/iwasm/fast-jit/fe/jit_emit_function.c
+++ core/iwasm/fast-jit/fe/jit_emit_function.c
@@ -331,52 +331,48 @@ jit_compile_op_call_indirect(JitCompContext *cc, uint32 type_idx,
                              uint32 tbl_idx)
 {
     JitReg elem_idx, native_ret, argv, arg_regs[6];
+    JitFrame *jit_frame = cc->jit_frame;
+    JitReg module_inst, tbl_size, elem_idx_long, offset;
+    JitReg func_idx, tbl_data, func_count_reg;
     WASMType *func_type;
 
     POP_I32(elem_idx);
 
-    // check elem_idx
+    /* check elem_idx */
+    module_inst = get_module_inst_reg(jit_frame);
+    tbl_size = get_table_cur_size_reg(jit_frame, tbl_idx);
 
-    JitFrame *jit_frame = cc->jit_frame;
-    // get module instance
-    JitReg module_inst = get_module_inst_reg(jit_frame);
-    // get table_instance
-    JitReg table_inst = get_table_inst_reg(jit_frame, tbl_idx);
-    // get table_cur_size
-    JitReg tbl_size = get_table_cur_size_reg(jit_frame, tbl_idx);
-    // tbl_sz = get_table_cur_size_reg(cc->jit_frame, tbl_idx);
     GEN_INSN(CMP, cc->cmp_reg, elem_idx, tbl_size);
     if (!jit_emit_exception(cc, JIT_EXCE_UNDEFINED_ELEMENT, JIT_OP_BGEU,
                             cc->cmp_reg, NULL))
         goto fail;
 
-    // sget func_idx and check if -1 or out of bounder
-
-    // get func_idx
-    JitReg elem_idx_long = jit_cc_new_reg_I64(cc);
+    /* check func_idx */
+    elem_idx_long = jit_cc_new_reg_I64(cc);
     GEN_INSN(I32TOI64, elem_idx_long, elem_idx);
 
-    JitReg offset = jit_cc_new_reg_I64(cc);
+    offset = jit_cc_new_reg_I64(cc);
     GEN_INSN(MUL, offset, elem_idx_long, NEW_CONST(I64, sizeof(uint32)));
-
-    JitReg func_idx = jit_cc_new_reg_I32(cc);
-    JitReg tbl_data = get_table_data_reg(jit_frame, tbl_idx);
+    
+    func_idx = jit_cc_new_reg_I32(cc);
+    tbl_data = get_table_data_reg(jit_frame, tbl_idx);
     GEN_INSN(LDI32, func_idx, tbl_data, offset);
+    // offset = jit_cc_new_reg_I32(cc);
+    // func_idx = jit_cc_new_reg_I32(cc);
+    // tbl_data = get_table_data_reg(jit_frame, tbl_idx);
 
-    // CMP func_idx with -1
+    // GEN_INSN(SHL, offset, elem_idx, 2);
+    // GEN_INSN(LDI32, func_idx, tbl_data, offset);
 
     GEN_INSN(CMP, cc->cmp_reg, func_idx, NEW_CONST(I32, -1));
-    if (!jit_emit_exception(cc, JIT_EXCE_INVALID_FUNCTION_INDEX, JIT_OP_BEQ,
+    if (!jit_emit_exception(cc, JIT_EXCE_UNINITIALIZED_ELEMENT, JIT_OP_BEQ,
                             cc->cmp_reg, NULL))
         goto fail;
 
-    // // get func_count_reg
-    JitReg fast_jit_func_ptrs_reg = get_fast_jit_func_ptrs_reg(jit_frame);
-    JitReg func_count_reg = jit_cc_new_reg_I32(cc);
+    func_count_reg = jit_cc_new_reg_I32(cc);
     GEN_INSN(LDI32, func_count_reg, module_inst,
              NEW_CONST(I32, offsetof(WASMModuleInstance, function_count)));
 
-    // CMP func_idx with func_count_reg
     GEN_INSN(CMP, cc->cmp_reg, func_idx, func_count_reg);
     if (!jit_emit_exception(cc, JIT_EXCE_INVALID_FUNCTION_INDEX, JIT_OP_BGTU,
                             cc->cmp_reg, NULL))
