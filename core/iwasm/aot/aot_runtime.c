2d36da3d9feccaa4904d7abc643226f9a5255d03
Shengyun Zhou <GGGZ-1101-28@live.cn>
2022-09-20 14:59:00 +0800

    Normalize the new code in last commit

--- core/iwasm/aot/aot_runtime.c
+++ core/iwasm/aot/aot_runtime.c
@@ -2917,11 +2917,11 @@ static const char *
 lookup_func_name(const char **func_names, uint32 *func_indexes,
                  uint32 func_index_count, uint32 func_index)
 {
-    if (!func_names || !func_indexes || func_index_count == 0)
-        return NULL;
     int64 low = 0, mid;
     int64 high = func_index_count - 1;
 
+    if (!func_names || !func_indexes || func_index_count == 0)
+        return NULL;
     while (low <= high) {
         mid = (low + high) / 2;
         if (func_index == func_indexes[mid]) {
