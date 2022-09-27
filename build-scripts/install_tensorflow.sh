107272bc2eb1d0d5cbe95d4e3fb37984cfd3601e
tonibofarull <toni.bofarull@midokura.com>
2022-09-27 14:37:57 +0200

    Same version

--- build-scripts/install_tensorflow.sh
+++ build-scripts/install_tensorflow.sh
@@ -4,5 +4,5 @@ cd /root/src/tflite
 if [ ! -d "tensorflow_src" ] 
 then
     git clone https://github.com/tensorflow/tensorflow.git tensorflow_src \
-        --branch v2.8.2 
+        --branch v2.10.0
 fi
