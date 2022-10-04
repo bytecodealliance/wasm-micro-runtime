80cf3b79f51652ac5a3b0a678c050c4bd252cebd
tonibofarull <toni.bofarull@midokura.com>
2022-10-04 17:58:33 +0200

    Parameterize

--- core/iwasm/libraries/wasi-nn/wasi_nn.cmake
+++ core/iwasm/libraries/wasi-nn/wasi_nn.cmake
@@ -6,3 +6,5 @@ set (WASI_NN_DIR ${CMAKE_CURRENT_LIST_DIR})
 add_definitions (-DWASM_ENABLE_WASI_NN=1)
 
 set (LIBC_WASI_NN_SOURCE ${WASI_NN_DIR}/wasi_nn_native.c ${WASI_NN_DIR}/wasi_nn_tensorflow.cpp)
+
+set (TENSORFLOW_LIB tensorflow-lite)
