d3e45d6fe75058ec727a44916d5023b24f85707c
tonibofarull <toni.bofarull@midokura.com>
2022-10-04 16:24:58 +0200

    Improve install tensorflow

--- core/deps/install_tensorflow.sh
+++ core/deps/install_tensorflow.sh
@@ -1,8 +1,3 @@
-CURR_PATH=$(cd $(dirname $0) && pwd -P)
-WAMR_ROOT_DIR=${CURR_PATH}/../..
-
-cd ${WAMR_ROOT_DIR}/core/deps
-
 if [ ! -d "tensorflow-src" ] 
 then
     git clone https://github.com/tensorflow/tensorflow.git tensorflow-src \
