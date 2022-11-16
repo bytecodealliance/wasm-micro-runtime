3ecc45fb97fe97f41850326dd8323a6414fd1516
Huang Qi <huangqi3@xiaomi.com>
2022-11-16 16:13:44 +0800

    Fix code style
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- product-mini/platforms/posix/main.c
+++ product-mini/platforms/posix/main.c
@@ -150,7 +150,9 @@ app_instance_repl(wasm_module_inst_t module_inst)
     size_t len = 0;
     ssize_t n;
 
-    while ((fflush(stdout), puts("webassembly> "), n = getline(&cmd, &len, stdin)) != -1) {
+    while (
+        (fflush(stdout), puts("webassembly> "), n = getline(&cmd, &len, stdin))
+        != -1) {
         bh_assert(n > 0);
         if (cmd[n - 1] == '\n') {
             if (n == 1)
