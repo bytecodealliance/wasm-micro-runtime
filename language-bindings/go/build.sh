b6f08d86f4997488dd7a486d81d22467b09cdb57
Nandor Kracser <bonifaido@gmail.com>
2023-01-09 14:23:29 +0100

    fix go language binding build on macos arm64

--- language-bindings/go/build.sh
+++ language-bindings/go/build.sh
@@ -7,6 +7,12 @@ PLATFORM=$(uname -s | tr A-Z a-z)
 CUR_DIR=$PWD
 WAMR_DIR=$PWD/../..
 WAMR_GO_DIR=$PWD/wamr
+ARCH=$(uname -m)
+if [ ${ARCH} = "arm64" ]; then
+    ARCH="aarch64"
+elif [ ${ARCH} = "x86_64" ]; then
+    ARCH="amd64"
+fi
 
 cp -a ${WAMR_DIR}/core/iwasm/include/*.h ${WAMR_GO_DIR}/packaged/include
 
@@ -15,7 +21,7 @@ cmake ${WAMR_DIR}/product-mini/platforms/${PLATFORM} \
     -DWAMR_BUILD_LIB_PTHREAD=1 -DWAMR_BUILD_DUMP_CALL_STACK=1 \
     -DWAMR_BUILD_MEMORY_PROFILING=1
 make -j ${nproc}
-cp -a libvmlib.a ${WAMR_GO_DIR}/packaged/lib/${PLATFORM}-amd64
+cp -a libvmlib.a ${WAMR_GO_DIR}/packaged/lib/${PLATFORM}-${ARCH}
 
 cd ${WAMR_GO_DIR}
 go test
