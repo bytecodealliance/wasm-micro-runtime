d8c333f32c71ec8e2b526e6e38cd6d9607c1b664
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-02-21 19:35:15 +0900

    Fix wamrapi python binding for darwin (#1978)

--- language-bindings/python/utils/create_lib.sh
+++ language-bindings/python/utils/create_lib.sh
@@ -6,7 +6,8 @@
 CUR_DIR=$(cd $(dirname $0) && pwd -P)
 ROOT_DIR=${CUR_DIR}/../../..
 
-WAMR_BUILD_PLATFORM=${WAMR_BUILD_PLATFORM:-"linux"}
+UNAME=$(uname -s|tr A-Z a-z)
+WAMR_BUILD_PLATFORM=${WAMR_BUILD_PLATFORM:-${UNAME}}
 
 cd ${ROOT_DIR}/product-mini/platforms/${WAMR_BUILD_PLATFORM}
 
@@ -14,4 +15,18 @@ mkdir -p build && cd build
 cmake ..
 make -j
 
-cp libiwasm.so ${CUR_DIR}/../src/wamr/libs
+case ${UNAME} in
+darwin)
+    LIBNAME=libiwasm.dylib
+    ;;
+*)
+    LIBNAME=libiwasm.so
+    ;;
+esac
+cp ${LIBNAME} ${CUR_DIR}/../src/wamr/libs
+
+cd ${ROOT_DIR}/language-bindings/python/src/wamr/wamrapi
+ctypesgen \
+${ROOT_DIR}/core/iwasm/include/wasm_export.h \
+-l ../libs/${LIBNAME} \
+-o iwasm.py
