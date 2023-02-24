fb8c4e51ab7238b78f48edbcdb8ff883f8aa7151
eloparco <eloparco@amazon.com>
2023-02-24 16:20:36 +0000

    feat(wasi-threads): execute WASI proposal tests in AOT mode

--- tests/wamr-test-suites/test_wamr.sh
+++ tests/wamr-test-suites/test_wamr.sh
@@ -495,17 +495,42 @@ function wasi_certification_test()
     cd wasi-testsuite
     git reset --hard ${WASI_TESTSUITE_COMMIT}
 
-    python3 -m venv wasi-env && source wasi-env/bin/activate
-    python3 -m pip install -r test-runner/requirements.txt
-    TEST_RUNTIME_EXE=${IWASM_CMD} python3 test-runner/wasi_test_runner.py \
-                -r adapters/wasm-micro-runtime.py \
-                -t \
-                    tests/c/testsuite/ \
-                    tests/assemblyscript/testsuite/ \
-                    tests/proposals/wasi-threads/ \
+    if [[ $1 != "aot" ]];then
+        python3 -m venv wasi-env && source wasi-env/bin/activate
+        python3 -m pip install -r test-runner/requirements.txt
+        TEST_RUNTIME_EXE="${IWASM_CMD}" python3 test-runner/wasi_test_runner.py \
+                    -r adapters/wasm-micro-runtime.py \
+                    -t \
+                        tests/c/testsuite/ \
+                        tests/assemblyscript/testsuite/ \
+                        tests/proposals/wasi-threads/ \
+                    | tee -a ${REPORT_DIR}/wasi_test_report.txt
+        exit_code=${PIPESTATUS[0]}
+        deactivate
+    else
+        # Run WASI thread proposal tests
+        exit_code=0
+        wasm_tests=$(ls tests/proposals/wasi-threads/*.wasm)
+        for test_wasm in ${wasm_tests}; do
+            test_aot="${test_wasm%.wasm}.aot"
+            test_json="${test_wasm%.wasm}.json"
+
+            echo "Compiling $test_wasm to $test_aot"
+            ${WAMRC_CMD} --enable-multi-thread -o $test_aot $test_wasm \
                 | tee -a ${REPORT_DIR}/wasi_test_report.txt
-    exit_code=${PIPESTATUS[0]}
-    deactivate
+
+            echo "Running $test_aot"
+            expected=$(jq .exit_code ${test_json})
+            ${IWASM_CMD} -v=5 $test_aot \
+                | tee -a ${REPORT_DIR}/wasi_test_report.txt
+            ret=${PIPESTATUS[0]}
+
+            echo "expected=$expected, actual=$ret"
+            if [[ $expected != "" ]] && [[ $expected != $ret ]];then
+                exit_code=1
+            fi
+        done
+    fi
 
     if [[ ${exit_code} -ne 0 ]];then
         echo -e "\nwasi tests FAILED" | tee -a ${REPORT_DIR}/wasi_test_report.txt
