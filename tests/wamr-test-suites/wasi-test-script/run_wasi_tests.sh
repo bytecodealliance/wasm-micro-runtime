e6ddf59aa18b39a1ba1326b7494ad4698d192475
eloparco <eloparco@amazon.com>
2023-03-01 23:06:09 +0000

    chore: move wasi thread test folder

--- tests/wamr-test-suites/wasi-test-script/run_wasi_tests.sh
+++ tests/wamr-test-suites/wasi-test-script/run_wasi_tests.sh
@@ -23,7 +23,7 @@ if [[ $MODE != "aot" ]];then
                     tests/c/testsuite/ \
                     tests/assemblyscript/testsuite/ \
                     tests/proposals/wasi-threads/ \
-                    ${WAMR_DIR}/core/iwasm/libraries/wasi-threads/test/
+                    ${WAMR_DIR}/core/iwasm/libraries/lib-wasi-threads/test/
     exit_code=${PIPESTATUS[0]}
     deactivate
 else
