c2dc4f7f3b00d51ccea0558f0a8e85e998aa53c6
Wenyong Huang <wenyong.huang@intel.com>
2023-03-07 04:19:29 +0800

    lock first then create thread

--- core/iwasm/libraries/thread-mgr/thread_manager.c
+++ core/iwasm/libraries/thread-mgr/thread_manager.c
@@ -583,6 +583,7 @@ thread_manager_start_routine(void *arg)
     /* Notify the parent thread to continue running */
     os_cond_signal(&exec_env->wait_cond);
     os_mutex_unlock(&exec_env->wait_lock);
+
     ret = exec_env->thread_start_routine(exec_env);
 
 #ifdef OS_ENABLE_HW_BOUND_CHECK
@@ -666,14 +667,16 @@ wasm_cluster_create_thread(WASMExecEnv *exec_env,
     new_exec_env->thread_start_routine = thread_routine;
     new_exec_env->thread_arg = arg;
 
+    os_mutex_lock(&new_exec_env->wait_lock);
+
     if (0
         != os_thread_create(&tid, thread_manager_start_routine,
                             (void *)new_exec_env,
                             APP_THREAD_STACK_SIZE_DEFAULT)) {
+        os_mutex_unlock(&new_exec_env->wait_lock);
         goto fail4;
     }
 
-    os_mutex_lock(&new_exec_env->wait_lock);
     /* Wait until the new_exec_env->handle is set to avoid it is
        illegally accessed after unlocking cluster->lock */
     os_cond_wait(&new_exec_env->wait_cond, &new_exec_env->wait_lock);
