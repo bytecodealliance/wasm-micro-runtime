79e71b22d034b555af374ff2901eeb4144df91fe
Wenyong Huang <wenyong.huang@intel.com>
2023-03-13 16:23:18 +0800

    Add benchmark libsodium

--- tests/benchmarks/coremark/build.sh
+++ tests/benchmarks/coremark/build.sh
@@ -13,7 +13,7 @@ cd coremark
 
 echo "Build coremark with gcc .."
 gcc -O3 -Iposix -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\" \
-        -DITERATIONS=0 -DPERFORMANCE_RUN=1 \
+        -DITERATIONS=400000 -DSEED_METHOD=SEED_VOLATILE -DPERFORMANCE_RUN=1 \
         core_list_join.c core_main.c core_matrix.c core_state.c \
         core_util.c posix/core_portme.c \
         -o ../coremark.exe -lrt
@@ -21,7 +21,7 @@ gcc -O3 -Iposix -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\" \
 echo "Build coremark with wasi-sdk .."
 /opt/wasi-sdk/bin/clang -O3 -Iposix -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1"\" \
         -Wl,--export=main \
-        -DITERATIONS=0 -DPERFORMANCE_RUN=1 \
+        -DITERATIONS=400000 -DSEED_METHOD=SEED_VOLATILE -DPERFORMANCE_RUN=1 \
         -Wl,--allow-undefined \
         core_list_join.c core_main.c core_matrix.c core_state.c \
         core_util.c posix/core_portme.c \
