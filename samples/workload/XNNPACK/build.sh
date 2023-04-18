e23d8cd1bd53361dda142ac248e35b576323b9c1
TL <tianlong.liang@intel.com>
2023-04-18 13:46:08 +0800

    build.sh for xnnpack

---
+++ samples/workload/XNNPACK/build.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+#######################################
+#   build xnnpack benchmark samples   #
+#######################################
+BUILD_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+WAMR_DIR="${BUILD_SCRIPT_DIR}/../../.."
+WAMR_PLATFORM_DIR="${WAMR_DIR}/product-mini/platforms"
+WAMRC_DIR="${WAMR_DIR}/wamr-compiler"
+OUT_DIR="${BUILD_SCRIPT_DIR}/out"
+
+set -xe
+
+# 1. build xnnpack wasm files
+# 1.1 to decide whether needs to build xnnpack wasm files
+cd ${WAMR_DIR}/samples/workload/XNNPACK
+xnnpack_out_dir=xnnpack/bazel-bin
+if [ -d "$xnnpack_out_dir" ]; then
+  echo "XNNPACK build directory exists"
+  file_count=$(ls "$xnnpack_out_dir" | grep "runfiles$" | wc -l)
+  # TODO: currently should be 116 files, may changes in the future
+  if [ $file_count -eq 116 ]; then
+    echo "Fully build XNNPACK benchmark wasm files"
+    need_build=false
+  else
+    echo "Partially build XNNPACK wasm files, rebuild again"
+    need_build=true
+  fi
+else
+  echo "XNNPACK build directory does not exist"
+  need_build=true
+fi
+
+# 1.2 only build wasm files if needed
+if [ "$need_build" = true ]; then
+  echo "Start building xnnpack"
+  # rm -fr build && mkdir build
+  # cmake -B build
+  # cmake --build build
+fi
+
+# 1.3 copy files to out/
+rm -rf ${OUT_DIR}
+mkdir ${OUT_DIR}
+# List all .wasm files in the input directory
+wasm_files=$(ls xnnpack/bazel-bin/*.runfiles/xnnpack/**.wasm)
+# Iterate through the .wasm files and create soft links
+for wasm_file in $wasm_files; do
+  # Get the filename without the path
+  wasm_name=$(basename "$wasm_file")
+  # Create a soft link in the output directory
+  ln -s "$wasm_file" "${out_dir}/${wasm_name}"
+done
+
+# 2. compile xnnpack benchmark wasm files to benchmark aot files with wamrc
+# 2.1 build wamr-compiler if needed
+cd ${WAMRC_DIR}
+./build_llvm.sh
+rm -fr build && mkdir build
+cd build && cmake ..
+make
+# 2.2 compile all .wasm files to .aot files
+cd build
+WAMRC_CMD="$(pwd)/wamrc"
+cd ${OUT_DIR}
+# List all .wasm files in the input directory
+wasm_files=$(ls *.wasm)
+# Iterate through the .wasm files and compile it to aot file
+for wasm_file in $wasm_files; do
+  # Get the filename without the path and the .wasm suffix
+  wasm_file_name=$(basename "$wasm_file" .wasm)
+  if [[ $1 == '--sgx' ]]; then
+    ${WAMRC_CMD} -sgx -o "${wasm_file_name}.aot" "${wasm_file_name}.wasm"
+  else
+    ${WAMRC_CMD} --enable-multi-thread -o "${wasm_file_name}.aot" "${wasm_file_name}.wasm"
+  fi
+done
+
+# 3. build iwasm with pthread and libc_emcc enable
+#    platform:
+#     linux by default
+#     linux-sgx if $1 equals '--sgx'
+if [[ $1 == '--sgx' ]]; then
+  cd ${WAMR_PLATFORM_DIR}/linux-sgx
+  rm -fr build && mkdir build
+  cd build && cmake .. -DWAMR_BUILD_LIBC_EMCC=1
+  make
+  cd ../enclave-sample
+  make
+else
+  cd ${WAMR_PLATFORM_DIR}/linux
+  rm -fr build && mkdir build
+  cd build && cmake .. -DWAMR_BUILD_LIB_PTHREAD=1 -DWAMR_BUILD_LIBC_EMCC=1
+  make
+fi
+
+# 4. run xnnpack with iwasm
+if [[ $1 == '--sgx' ]]; then
+  IWASM_CMD="${WAMR_PLATFORM_DIR}/linux-sgx/enclave-sample/iwasm"
+else
+  IWASM_CMD="${WAMR_PLATFORM_DIR}/linux/build/iwasm"
+fi
+cd ${OUT_DIR}
+# List all .wasm files in the input directory
+aot_files=$(ls *.aot)
+# Iterate through the .wasm files and compile it to aot file
+for aot_file in $aot_files; do
+  echo "---> run xnnpack benchmark $aot_file iwasm"
+  if [[ $1 == '--sgx' ]]; then
+    ${IWASM_CMD} $aot_file
+  fi
+done
