fc843479fe23298e563be1db4ac92aa3b6359291
ayakoakasaka <ayako.akasaka@midokura.com>
2023-04-25 05:38:23 +0200

    set include path only when tflite is detected

--- core/iwasm/libraries/wasi-nn/cmake/Findtensorflow-lite.cmake
+++ core/iwasm/libraries/wasi-nn/cmake/Findtensorflow-lite.cmake
@@ -6,16 +6,6 @@ find_library(TENSORFLOW_LITE
      NAMES tensorflow-lite
      HINTS ${CMAKE_LIBRARY_PATH}
 )
-find_path(TENSORFLOW_LITE_INCLUDE_DIR
-     NAMES tensorflow/lite/interpreter.h
-     HINTS ${CMAKE_LIBRARY_PATH}../include
-)
-find_path(FLATBUFFER_INCLUDE_DIR
-     NAMES flatbuffers/flatbuffers.h
-     HINTS ${CMAKE_LIBRARY_PATH}../include
-)
-include_directories (${TENSORFLOW_LITE_INCLUDE_DIR})
-include_directories (${FLATBUFFER_INCLUDE_DIR})
 
 if(NOT EXISTS ${TENSORFLOW_LITE})
     if (NOT EXISTS "${WAMR_ROOT_DIR}/core/deps/tensorflow-src")
@@ -37,5 +27,16 @@ if(NOT EXISTS ${TENSORFLOW_LITE})
     # * https://www.tensorflow.org/lite/guide/build_cmake#available_options_to_build_tensorflow_lite
     set (TFLITE_ENABLE_GPU ON)
     endif ()
+else()
+    find_path(TENSORFLOW_LITE_INCLUDE_DIR
+    NAMES tensorflow/lite/interpreter.h
+    HINTS ${CMAKE_LIBRARY_PATH}../include
+    )
+    find_path(FLATBUFFER_INCLUDE_DIR
+    NAMES flatbuffers/flatbuffers.h
+    HINTS ${CMAKE_LIBRARY_PATH}../include
+    )
+    include_directories (${TENSORFLOW_LITE_INCLUDE_DIR})
+    include_directories (${FLATBUFFER_INCLUDE_DIR})    
 endif()
 
