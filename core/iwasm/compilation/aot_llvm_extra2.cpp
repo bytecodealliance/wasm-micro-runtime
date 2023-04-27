61ba2b40bac65a82306ac417a9a585d04ddcfae6
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-04-27 11:32:43 +0900

    aot_llvm_extra2.cpp: apply clang-format for this repository

--- core/iwasm/compilation/aot_llvm_extra2.cpp
+++ core/iwasm/compilation/aot_llvm_extra2.cpp
@@ -8,60 +8,60 @@
 static llvm::Optional<llvm::Reloc::Model>
 convert(LLVMRelocMode reloc_mode)
 {
-        switch (reloc_mode) {
+    switch (reloc_mode) {
         case LLVMRelocDefault:
-                return llvm::None;
+            return llvm::None;
         case LLVMRelocStatic:
-                return llvm::Reloc::Static;
+            return llvm::Reloc::Static;
         case LLVMRelocPIC:
-                return llvm::Reloc::PIC_;
+            return llvm::Reloc::PIC_;
         case LLVMRelocDynamicNoPic:
-                return llvm::Reloc::DynamicNoPIC;
+            return llvm::Reloc::DynamicNoPIC;
         case LLVMRelocROPI:
-                return llvm::Reloc::ROPI;
+            return llvm::Reloc::ROPI;
         case LLVMRelocRWPI:
-                return llvm::Reloc::RWPI;
+            return llvm::Reloc::RWPI;
         case LLVMRelocROPI_RWPI:
-                return llvm::Reloc::ROPI_RWPI;
-        }
+            return llvm::Reloc::ROPI_RWPI;
+    }
 }
 
 static llvm::CodeGenOpt::Level
 convert(LLVMCodeGenOptLevel opt_level)
 {
-        switch (opt_level) {
+    switch (opt_level) {
         case LLVMCodeGenLevelNone:
-                return llvm::CodeGenOpt::None;
+            return llvm::CodeGenOpt::None;
         case LLVMCodeGenLevelLess:
-                return llvm::CodeGenOpt::Less;
+            return llvm::CodeGenOpt::Less;
         case LLVMCodeGenLevelDefault:
-                return llvm::CodeGenOpt::Default;
+            return llvm::CodeGenOpt::Default;
         case LLVMCodeGenLevelAggressive:
-                return llvm::CodeGenOpt::Aggressive;
-        }
+            return llvm::CodeGenOpt::Aggressive;
+    }
 }
 
 static llvm::Optional<llvm::CodeModel::Model>
 convert(LLVMCodeModel code_model, bool *jit)
 {
-        *jit = false;
-        switch (code_model) {
+    *jit = false;
+    switch (code_model) {
         case LLVMCodeModelDefault:
-                return llvm::None;
+            return llvm::None;
         case LLVMCodeModelJITDefault:
-                *jit = true;
-                return llvm::None;
+            *jit = true;
+            return llvm::None;
         case LLVMCodeModelTiny:
-                return llvm::CodeModel::Tiny;
+            return llvm::CodeModel::Tiny;
         case LLVMCodeModelSmall:
-                return llvm::CodeModel::Small;
+            return llvm::CodeModel::Small;
         case LLVMCodeModelKernel:
-                return llvm::CodeModel::Kernel;
+            return llvm::CodeModel::Kernel;
         case LLVMCodeModelMedium:
-                return llvm::CodeModel::Medium;
+            return llvm::CodeModel::Medium;
         case LLVMCodeModelLarge:
-                return llvm::CodeModel::Large;
-        }
+            return llvm::CodeModel::Large;
+    }
 }
 
 LLVMTargetMachineRef
@@ -73,24 +73,24 @@ LLVMCreateTargetMachineWithOpts(LLVMTargetRef ctarget, const char *triple,
                                 bool EmitStackSizeSection,
                                 const char *StackUsageOutput)
 {
-        llvm::TargetOptions opts;
+    llvm::TargetOptions opts;
 
-        // -fstack-size-section equiv
-        // emit it to ".stack_sizes" section in case of ELF
-        // you can read it with "llvm-readobj --stack-sizes"
-        opts.EmitStackSizeSection = EmitStackSizeSection;
+    // -fstack-size-section equiv
+    // emit it to ".stack_sizes" section in case of ELF
+    // you can read it with "llvm-readobj --stack-sizes"
+    opts.EmitStackSizeSection = EmitStackSizeSection;
 
-        // -fstack-usage equiv
-        if (StackUsageOutput != NULL) {
-                opts.StackUsageOutput = StackUsageOutput;
-        }
+    // -fstack-usage equiv
+    if (StackUsageOutput != NULL) {
+        opts.StackUsageOutput = StackUsageOutput;
+    }
 
-        auto target = reinterpret_cast<llvm::Target *>(ctarget);
-        auto rm = convert(reloc_mode);
-        auto ol = convert(opt_level);
-        bool jit;
-        auto cm = convert(code_model, &jit);
-        auto targetmachine = target->createTargetMachine(
-                triple, cpu, features, opts, rm, cm, ol, jit);
-        return reinterpret_cast<LLVMTargetMachineRef>(targetmachine);
+    auto target = reinterpret_cast<llvm::Target *>(ctarget);
+    auto rm = convert(reloc_mode);
+    auto ol = convert(opt_level);
+    bool jit;
+    auto cm = convert(code_model, &jit);
+    auto targetmachine = target->createTargetMachine(triple, cpu, features,
+                                                     opts, rm, cm, ol, jit);
+    return reinterpret_cast<LLVMTargetMachineRef>(targetmachine);
 }
