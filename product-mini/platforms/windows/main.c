17b68253914720f0a4c46664d4f0061d861f7746
eloparco <eloparco@amazon.com>
2023-05-02 15:40:38 +0100

    fix: return error when exception encountered after main thread finishes

--- product-mini/platforms/windows/main.c
+++ product-mini/platforms/windows/main.c
@@ -549,8 +549,12 @@ main(int argc, char *argv[])
 
 #if WASM_ENABLE_LIBC_WASI != 0
     if (ret == 0) {
-        /* propagate wasi exit code. */
+        /* wait for threads to finish and propagate wasi exit code. */
         ret = wasm_runtime_get_wasi_exit_code(wasm_module_inst);
+        if (wasm_runtime_get_exception(wasm_module_inst)) {
+            /* got an exception in spawned thread */
+            ret = 1;
+        }
     }
 #endif
 
