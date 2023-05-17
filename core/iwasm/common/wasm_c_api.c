e189dcbc9fabebd17eb46aeee645b0004104e5b2
Huang Qi <huangqi3@xiaomi.com>
2023-05-17 09:49:52 +0800

    GC AOT: Change func_types to types in AOTModule (#2217)

--- core/iwasm/common/wasm_c_api.c
+++ core/iwasm/common/wasm_c_api.c
@@ -3010,9 +3010,9 @@ wasm_func_new_internal(wasm_store_t *store, uint16 func_idx_rt,
         }
         else {
             type_rt =
-                module_aot->func_types[module_aot->func_type_indexes
-                                           [func_idx_rt
-                                            - module_aot->import_func_count]];
+                module_aot
+                    ->types[module_aot->func_type_indexes
+                                [func_idx_rt - module_aot->import_func_count]];
         }
     }
 #endif
