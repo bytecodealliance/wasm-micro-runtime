8ef09be604bc61fa859fe38ad139f5c73f652e03
Wenyong Huang <wenyong.huang@intel.com>
2023-06-06 08:33:15 +0800

    Fix compile error of wamrc with llvm-13/llvm-14 (#2261)

--- core/iwasm/compilation/aot_llvm_extra.cpp
+++ core/iwasm/compilation/aot_llvm_extra.cpp
@@ -348,12 +348,16 @@ aot_apply_llvm_new_pass_manager(AOTCompContext *comp_ctx, LLVMModuleRef module)
         FPM.addPass(LoadStoreVectorizerPass());
 
         if (comp_ctx->enable_llvm_pgo || comp_ctx->use_prof_file) {
-            LICMOptions licm_opt;
             /* LICM pass: loop invariant code motion, attempting to remove
                as much code from the body of a loop as possible. Experiments
                show it is good to enable it when pgo is enabled. */
+#if LLVM_VERSION_MAJOR >= 15
+            LICMOptions licm_opt;
             FPM.addPass(
                 createFunctionToLoopPassAdaptor(LICMPass(licm_opt), true));
+#else
+            FPM.addPass(createFunctionToLoopPassAdaptor(LICMPass(), true));
+#endif
         }
 
         /*
