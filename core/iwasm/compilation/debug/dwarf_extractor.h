cabcb177c88dac1961c69ac33026fab5794ec024
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-06-09 10:52:03 +0900

    dwarf_extractor: Constify a bit (#2278)

--- core/iwasm/compilation/debug/dwarf_extractor.h
+++ core/iwasm/compilation/debug/dwarf_extractor.h
@@ -30,24 +30,26 @@ dwar_extractor_handle_t
 create_dwarf_extractor(aot_comp_data_t comp_data, char *file_name);
 
 LLVMMetadataRef
-dwarf_gen_file_info(AOTCompContext *comp_ctx);
+dwarf_gen_file_info(const AOTCompContext *comp_ctx);
 
 LLVMMetadataRef
-dwarf_gen_comp_unit_info(AOTCompContext *comp_ctx);
+dwarf_gen_comp_unit_info(const AOTCompContext *comp_ctx);
 
 LLVMMetadataRef
-dwarf_gen_func_info(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx);
+dwarf_gen_func_info(const AOTCompContext *comp_ctx,
+                    const AOTFuncContext *func_ctx);
 
 LLVMMetadataRef
-dwarf_gen_location(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx,
-                   uint64_t vm_offset);
+dwarf_gen_location(const AOTCompContext *comp_ctx,
+                   const AOTFuncContext *func_ctx, uint64_t vm_offset);
 
 LLVMMetadataRef
-dwarf_gen_func_ret_location(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx);
+dwarf_gen_func_ret_location(const AOTCompContext *comp_ctx,
+                            const AOTFuncContext *func_ctx);
 
 void
-dwarf_get_func_name(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx,
-                    char *name, int len);
+dwarf_get_func_name(const AOTCompContext *comp_ctx,
+                    const AOTFuncContext *func_ctx, char *name, int len);
 
 #ifdef __cplusplus
 }
