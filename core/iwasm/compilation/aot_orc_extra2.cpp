c8d05f86f2bdbdcd16c57c37a4c2ab6f3b5b082a
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-06-09 16:28:53 +0900

    copyright comment

--- core/iwasm/compilation/aot_orc_extra2.cpp
+++ core/iwasm/compilation/aot_orc_extra2.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Midokura Japan KK.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
