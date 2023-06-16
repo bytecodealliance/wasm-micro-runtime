3881a77b166f511abd4532c4cb9ccd7d297ef6f5
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-06-16 16:10:07 -0700

    PR review

--- core/iwasm/libraries/libc-uvwasi/FindLIBUV.cmake
+++ core/iwasm/libraries/libc-uvwasi/FindLIBUV.cmake
@@ -1,11 +1,14 @@
+# Copyright (C) 2023 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 # Find libuv library
 # This module defines
 #  LIBUV_FOUND, if false, do not try to link to libuv
-#  UV_A_LIBS
+#  LIBUV_LIBRARIES
 #  LIBUV_INCLUDE_DIR, where to find uv.h
 
 find_path(LIBUV_INCLUDE_DIR NAMES uv.h)
-find_library(UV_A_LIBS NAMES uv libuv)
+find_library(LIBUV_LIBRARIES NAMES uv libuv)
 
 include(FindPackageHandleStandardArgs)
 
@@ -13,13 +16,13 @@ find_package_handle_standard_args(
   LIBUV
   FOUND_VAR LIBUV_FOUND
   REQUIRED_VARS
-    UV_A_LIBS
+    LIBUV_LIBRARIES
     LIBUV_INCLUDE_DIR
 )
 
 if(WIN32)
-  list(APPEND UV_A_LIBS iphlpapi)
-  list(APPEND UV_A_LIBS psapi)
-  list(APPEND UV_A_LIBS userenv)
-  list(APPEND UV_A_LIBS ws2_32)
+  list(APPEND LIBUV_LIBRARIES iphlpapi)
+  list(APPEND LIBUV_LIBRARIES psapi)
+  list(APPEND LIBUV_LIBRARIES userenv)
+  list(APPEND LIBUV_LIBRARIES ws2_32)
 endif()
--- core/iwasm/libraries/libc-uvwasi/FindUVWASI.cmake
+++ core/iwasm/libraries/libc-uvwasi/FindUVWASI.cmake
@@ -1,11 +1,14 @@
+# Copyright (C) 2023 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 # Find libuvwasi library
 # This module defines
 #  UVWASI_FOUND, if false, do not try to link to libuvwasi
-#  UVWASI_LIBS
+#  UVWASI_LIBRARIES
 #  UVWASI_INCLUDE_DIR, where to find headers
 
 find_path(UVWASI_INCLUDE_DIR NAMES uvwasi.h wasi_serdes.h wasi_types.h PATH_SUFFIXES uvwasi)
-find_library(UVWASI_LIBS NAMES uvwasi_a)
+find_library(UVWASI_LIBRARIES NAMES uvwasi_a)
 
 include(FindPackageHandleStandardArgs)
 
@@ -13,7 +16,7 @@ find_package_handle_standard_args(
   UVWASI
   FOUND_VAR UVWASI_FOUND
   REQUIRED_VARS
-    UVWASI_LIBS
+    UVWASI_LIBRARIES
     UVWASI_INCLUDE_DIR
 )
 
