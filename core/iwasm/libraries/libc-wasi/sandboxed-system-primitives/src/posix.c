875cb59c678b619d4d12449366c60f0e3f45fc0f
victoryang00 <victoryang00@ucsc.edu>
2023-06-27 12:37:18 -0700

    integrate windows support 2

--- core/iwasm/libraries/libc-wasi/sandboxed-system-primitives/src/posix.c
+++ core/iwasm/libraries/libc-wasi/sandboxed-system-primitives/src/posix.c
@@ -1576,7 +1576,9 @@ path_get(struct fd_table *curfds, struct path_access *pa, __wasi_fd_t fd,
     pa->path = pa->path_start = path;
     pa->follow = (flags & __WASI_LOOKUP_SYMLINK_FOLLOW) != 0;
     pa->fd_object = fo;
+#if WASM_BUILD_CHECKPOINT_RESTORE != 0 
     insert_fd(pa->fd, path, 0);
+#endif
     return 0;
 #else
     // The implementation provides no mechanism to constrain lookups to a
@@ -1765,7 +1767,9 @@ success:
     pa->fd = fds[curfd];
     pa->follow = false;
     pa->fd_object = fo;
+#if WASM_ENABLE_CHECKPOINT_RESTORE != 0
     insert_fd(pa->fd, path, 0);
+#endif
     return 0;
 
 fail:
