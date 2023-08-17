4ce675aacd7d69664c74f9daaf7badab38c4c80f
Maks Litskevich <makslit@amazon.com>
2023-08-17 03:31:05 +0100

    Add another wamr test (#2411)
    Follows up #2364 where we discussed that we might want to have a test
    which has really short thread function and creates many threads.

--- core/iwasm/libraries/lib-wasi-threads/test/build.sh
+++ core/iwasm/libraries/lib-wasi-threads/test/build.sh
@@ -34,7 +34,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Stress tests names
-thread_start_file_exclusions=("spawn_stress_test.wasm" "linear_memory_size_update.wasm")
+thread_start_file_exclusions=("spawn_stress_test.wasm" "linear_memory_size_update.wasm" "stress_test_threads_creation.wasm")
 
 for test_c in *.c; do
     test_wasm="$(basename $test_c .c).wasm"
@@ -56,6 +56,7 @@ for test_c in *.c; do
     echo "Compiling $test_c to $test_wasm"
     $CC \
         -target wasm32-wasi-threads \
+        -O2 \
         -pthread -ftls-model=local-exec \
         -z stack-size=32768 \
         -Wl,--export=__heap_base \
