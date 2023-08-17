79d457205e8725cc3d14a648e03e42cf8381ccb6
Xu Jun <jun1.xu@intel.com>
2023-08-17 08:53:34 +0800

    fix opcode overwrite issue in fast interp

--- core/iwasm/interpreter/wasm_loader.c
+++ core/iwasm/interpreter/wasm_loader.c
@@ -8064,6 +8064,7 @@ re_scan:
             case WASM_OP_SELECT_T:
             {
                 uint8 vec_len, ref_type;
+                uint8 *p_code_compiled_orig = loader_ctx->p_code_compiled;
 
                 read_leb_uint32(p, p_end, vec_len);
                 if (vec_len != 1) {
@@ -8086,8 +8087,7 @@ re_scan:
 #if WASM_ENABLE_FAST_INTERP != 0
                 if (loader_ctx->p_code_compiled) {
                     uint8 opcode_tmp = WASM_OP_SELECT;
-                    uint8 *p_code_compiled_tmp =
-                        loader_ctx->p_code_compiled - 2;
+                    uint8 *p_code_compiled_tmp = p_code_compiled_orig;
 
                     if (ref_type == VALUE_TYPE_V128) {
 #if (WASM_ENABLE_SIMD == 0) \
