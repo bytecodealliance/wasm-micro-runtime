8c2dc1d011b39950a4fd29ae9b032b1c1424a6e1
Marcin Kolny <marcin.kolny@gmail.com>
2023-09-04 06:21:10 +0100

    Add unit tests for the tid allocator (#2519)
    Add simple infrastructure to add more unit tests in the future. At the moment tests
    are only executed on Linux, but can be extended to other platforms if needed.
    
    Use https://github.com/google/googletest/ as a framework.

--- core/iwasm/libraries/lib-wasi-threads/tid_allocator.h
+++ core/iwasm/libraries/lib-wasi-threads/tid_allocator.h
@@ -8,8 +8,14 @@
 
 #include "platform_common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define TID_ALLOCATOR_INIT_SIZE CLUSTER_MAX_THREAD_NUM
 enum {
+    /* Keep it in sync with
+       https://github.com/WebAssembly/wasi-threads#design-choice-thread-ids */
     TID_MIN = 1,
     TID_MAX = 0x1FFFFFFF
 }; // Reserved TIDs (WASI specification)
@@ -32,5 +38,9 @@ tid_allocator_get_tid(TidAllocator *tid_allocator);
 
 void
 tid_allocator_release_tid(TidAllocator *tid_allocator, int32 thread_id);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _TID_ALLOCATOR_H */
