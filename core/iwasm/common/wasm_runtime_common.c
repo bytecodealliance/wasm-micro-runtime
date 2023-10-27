97708759f920ccd7ccd64e1e9f2bb138a44d3cc7
Wenyong Huang <wenyong.huang@intel.com>
2023-10-27 12:23:51 +0800

    Update wasm_runtime_common.c
    Add `if (mapping_copy != mapping_copy_buf) wasm_runtime_free(mapping_copy)` before `goto fail`.

--- core/iwasm/common/wasm_runtime_common.c
+++ core/iwasm/common/wasm_runtime_common.c
@@ -3047,6 +3047,8 @@ wasm_runtime_init_wasi(WASMModuleInstanceCommon *module_inst,
                 snprintf(error_buf, error_buf_size,
                          "error while pre-opening mapped directory: "
                          "invalid map\n");
+            if (mapping_copy != mapping_copy_buf)
+                wasm_runtime_free(mapping_copy);
             goto fail;
         }
 
