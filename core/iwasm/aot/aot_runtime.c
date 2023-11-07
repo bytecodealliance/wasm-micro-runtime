424c245ad21e0194dfe1555e78a050d20558bbfa
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-11-07 15:10:44 +0900

    aot: use wasm_exec_env_restore_module_inst in one more place

--- core/iwasm/aot/aot_runtime.c
+++ core/iwasm/aot/aot_runtime.c
@@ -971,7 +971,8 @@ execute_post_instantiate_functions(AOTModuleInstance *module_inst,
            wasm functions, and ensure that the exec_env's module inst
            is the correct one. */
         module_inst_main = exec_env_main->module_inst;
-        exec_env->module_inst = (WASMModuleInstanceCommon *)module_inst;
+        wasm_exec_env_set_module_inst(exec_env,
+                                      (WASMModuleInstanceCommon *)module_inst);
     }
     else {
         /* Try using the existing exec_env */
@@ -1047,7 +1048,7 @@ execute_post_instantiate_functions(AOTModuleInstance *module_inst,
 fail:
     if (is_sub_inst) {
         /* Restore the parent exec_env's module inst */
-        exec_env_main->module_inst = module_inst_main;
+        wasm_exec_env_restore_module_inst(exec_env_main, module_inst_main);
     }
     else {
         if (module_inst_main)
