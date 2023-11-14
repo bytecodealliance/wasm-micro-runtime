619086fd2b3d4f5c7f480217fd279fb171ce0331
Wenyong Huang <wenyong.huang@intel.com>
2023-11-14 20:36:51 +0800

    Disable heap corruption check in ems memory allocator by default

--- core/config.h
+++ core/config.h
@@ -315,6 +315,16 @@
 #define BH_ENABLE_GC_VERIFY 0
 #endif
 
+/* Enable heap corruption check if heap verification is enabled */
+#if BH_ENABLE_GC_VERIFY != 0
+#define BH_ENABLE_GC_CORRUPTION_CHECK 1
+#endif
+
+/* Heap corruption check */
+#ifndef BH_ENABLE_GC_CORRUPTION_CHECK
+#define BH_ENABLE_GC_CORRUPTION_CHECK 0
+#endif
+
 /* Enable global heap pool if heap verification is enabled */
 #if BH_ENABLE_GC_VERIFY != 0
 #define WASM_ENABLE_GLOBAL_HEAP_POOL 1
--- core/shared/mem-alloc/mem_alloc.cmake
+++ core/shared/mem-alloc/mem_alloc.cmake
@@ -10,6 +10,10 @@ if (WAMR_BUILD_GC_VERIFY EQUAL 1)
     add_definitions (-DBH_ENABLE_GC_VERIFY=1)
 endif ()
 
+if (WAMR_BUILD_GC_CORRUPTION_CHECK EQUAL 1)
+    add_definitions (-DBH_ENABLE_GC_CORRUPTION_CHECK=1)
+endif ()
+
 file (GLOB_RECURSE source_all
       ${MEM_ALLOC_DIR}/ems/*.c
       ${MEM_ALLOC_DIR}/tlsf/*.c
