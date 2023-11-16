dd0556a7295b5707c7d3e995400e40ac78dcfa67
Wenyong Huang <wenyong.huang@intel.com>
2023-11-16 11:05:23 +0800

    aot compiler: Disable musttail for thumb (#2771)
    Error is reported when executing `wamrc --target=thumb -o <aot_file> <wasm_file>`:
    ```
    LLVM ERROR: failed to perform tail call elimination on a call site marked musttail
    Aborted (core dumped)
    ```

--- core/iwasm/compilation/aot_llvm.c
+++ core/iwasm/compilation/aot_llvm.c
@@ -153,6 +153,13 @@ aot_target_precheck_can_use_musttail(const AOTCompContext *comp_ctx)
          */
         return false;
     }
+    if (strstr(comp_ctx->target_arch, "thumb")) {
+        /*
+         * cf.
+         * https://github.com/bytecodealliance/wasm-micro-runtime/issues/2412
+         */
+        return false;
+    }
     /*
      * x86-64/i386: true
      *
