ea4ff91e209b6a00613875f64e91a067ee2237fb
Brian <89487381+b4yuan@users.noreply.github.com>
2023-11-22 22:30:12 -0500

    build wamrc

--- .github/workflows/codeql_buildscript.sh
+++ .github/workflows/codeql_buildscript.sh
@@ -2,6 +2,15 @@
 
 sudo apt install build-essential cmake g++-multilib libgcc-9-dev lib32gcc-9-dev ccache
 
+cd wamr-compiler
+./build_llvm.sh 
+mkdir build && cd build
+cmake .. 
+make
+# wamrc is generated under current directory
+
+cd ../..
+
 cd product-mini/platforms/linux/
 mkdir build && cd build
 cmake ..
