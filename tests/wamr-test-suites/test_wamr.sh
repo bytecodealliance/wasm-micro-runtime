bf3978dd1edf092284c47817d139297a4bd3166e
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-12-01 17:49:40 +0900

    tests/wamr-test-suites/test_wamr.sh: don't bother to build shared library
    This script only uses the iwasm command.

--- tests/wamr-test-suites/test_wamr.sh
+++ tests/wamr-test-suites/test_wamr.sh
@@ -697,7 +697,7 @@ function build_iwasm_with_cfg()
         && if [ -d build ]; then rm -rf build/*; else mkdir build; fi \
         && cd build \
         && cmake $* .. \
-        && make -j 4
+        && make -j 4 iwasm
         cd ${WAMR_DIR}/product-mini/platforms/linux-sgx/enclave-sample \
         && make clean \
         && make SPEC_TEST=1
@@ -706,7 +706,7 @@ function build_iwasm_with_cfg()
         && if [ -d build ]; then rm -rf build/*; else mkdir build; fi \
         && cd build \
         && cmake $* .. \
-        && cmake --build . -j 4 --config RelWithDebInfo
+        && cmake --build . -j 4 --config RelWithDebInfo --target iwasm
     fi
 
     if [ "$?" != 0 ];then
