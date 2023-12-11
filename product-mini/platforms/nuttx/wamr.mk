6236e985e4a2d87b3415c353232c932e91ec3a74
Huang Qi <huangqi3@xiaomi.com>
2023-12-11 15:35:43 +0800

    Enable GC spectest for NuttX

--- product-mini/platforms/nuttx/wamr.mk
+++ product-mini/platforms/nuttx/wamr.mk
@@ -311,7 +311,7 @@ endif
 
 ifeq ($(CONFIG_INTERPRETERS_WAMR_GC),y)
 CFLAGS += -DWASM_ENABLE_GC=1
-CSRCS += gc_type.c gc_object.c
+CSRCS += gc_common.c gc_type.c gc_object.c
 VPATH += $(IWASM_ROOT)/common/gc
 else
 CFLAGS += -DWASM_ENABLE_GC=0
