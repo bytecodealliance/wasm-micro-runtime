552177d04b46e0b90a49707bb10c4a827da2fbfe
Wenyong Huang <wenyong.huang@intel.com>
2024-01-09 14:27:38 +0800

    code style

--- core/shared/platform/esp-idf/shared_platform.cmake
+++ core/shared/platform/esp-idf/shared_platform.cmake
@@ -11,7 +11,7 @@ include_directories(${PLATFORM_SHARED_DIR}/../include)
 file (GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
 
 include (${CMAKE_CURRENT_LIST_DIR}/../common/libc-util/platform_common_libc_util.cmake)
-set(source_all ${source_all} ${PLATFORM_COMMON_LIBC_UTIL_SOURCE})
+set (source_all ${source_all} ${PLATFORM_COMMON_LIBC_UTIL_SOURCE})
 
 set (PLATFORM_SHARED_SOURCE ${source_all} ${PLATFORM_COMMON_MATH_SOURCE})
 
