5781ea87a381057afdd100cb14aec8b278835905
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-01-12 13:06:16 +0900

    aot debug: try to use a bit more approprirate file names
    when the original wasm contains multiple compilation units,
    the current logic uses the first one for everything.
    this commit tries to use a bit more appropriate ones.

--- core/iwasm/compilation/debug/dwarf_extractor.cpp
+++ core/iwasm/compilation/debug/dwarf_extractor.cpp
@@ -298,7 +298,7 @@ lldb_function_to_function_dbi(const AOTCompContext *comp_ctx,
         return NULL;
 
     LLVMDIBuilderRef DIB = comp_ctx->debug_builder;
-    LLVMMetadataRef File = comp_ctx->debug_file;
+    LLVMMetadataRef File = comp_ctx->debug_file; /* a fallback */
 
     LLVMMetadataRef ParamTypes[num_function_args + 1];
 
@@ -313,6 +313,20 @@ lldb_function_to_function_dbi(const AOTCompContext *comp_ctx,
             ParamTypes[function_arg_idx + 1] =
                 lldb_type_to_type_dbi(comp_ctx, function_arg_type);
         }
+    }
+
+    auto compile_unit = sc.GetCompileUnit();
+    auto file_spec = compile_unit.GetFileSpec();
+    const char *file_name = file_spec.GetFilename();
+    const char *dir_name = file_spec.GetDirectory();
+    LLVMMetadataRef file_info = NULL;
+    if (file_name || dir_name) {
+        file_info = LLVMDIBuilderCreateFile(comp_ctx->debug_builder, file_name,
+                                            strlen(file_name), dir_name,
+                                            strlen(dir_name));
+    }
+    if (file_info) {
+        File = file_info;
     }
 
     LLVMMetadataRef FunctionTy = LLVMDIBuilderCreateSubroutineType(
