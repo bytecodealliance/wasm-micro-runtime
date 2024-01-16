85b6f167c982a643d4c75f096b509f46377be456
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-01-16 14:37:20 +0900

    Revert "Enable MAP_32BIT for macOS (#2992)"
    This reverts commit 6fa6d6d9a585696d171905edbb2605e0debcb5cf.
    
    discussion: https://github.com/bytecodealliance/wasm-micro-runtime/issues/3009

--- build-scripts/config_common.cmake
+++ build-scripts/config_common.cmake
@@ -473,8 +473,3 @@ else ()
   # Disable quick aot/jit entries for interp and fast-jit
   add_definitions (-DWASM_ENABLE_QUICK_AOT_ENTRY=0)
 endif ()
-if (APPLE)
-  # On recent macOS versions, by default, the size of page zero is 4GB.
-  # Shrink it to make MAP_32BIT mmap can work.
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-pagezero_size,0x4000")
-endif ()
