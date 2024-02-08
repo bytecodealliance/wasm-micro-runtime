0ba8ce6583dfd329155547b74abf2ebe622d84ce
Wenyong Huang <wenyong.huang@intel.com>
2024-02-08 11:55:24 +0800

    shared-platform: Remove dependency on shared-utils' bh_memory_remap_slow

--- core/shared/platform/common/memory/mremap.c
+++ core/shared/platform/common/memory/mremap.c
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
-#include "bh_memutils.h"
+#include "platform_api_vmcore.h"
 
 void *
 os_mremap(void *old_addr, size_t old_size, size_t new_size)
 {
-    return bh_memory_remap_slow(old_addr, old_size, new_size);
+    return os_mremap_slow(old_addr, old_size, new_size);
 }
