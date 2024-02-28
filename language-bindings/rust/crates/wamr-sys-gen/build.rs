e0c0fa788bf8304cd4905735915adc45a4fdb9b1
liang.he@intel.com <liang.he@intel.com>
2024-02-28 03:02:32 +0000

    Rename wamr-sys to wamr-sys-gen
    - keep wamr-sys-gen in wamr repo to generate bindings.rs
    - separae wamr-sys and wamr-rust-sdk

---
+++ language-bindings/rust/crates/wamr-sys-gen/build.rs
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 Liquid Reply GmbH. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+extern crate bindgen;
+
+use std::{env, path::Path, path::PathBuf};
+
+fn main() {
+    let wamr_root = Path::new("../../../../").canonicalize().unwrap();
+    assert!(wamr_root.exists());
+
+    let wamr_header = wamr_root.join("core/iwasm/include/wasm_export.h");
+    assert!(wamr_header.exists());
+
+    let bindings = bindgen::Builder::default()
+        .ctypes_prefix("::core::ffi")
+        .use_core()
+        .header(wamr_header.into_os_string().into_string().unwrap())
+        .derive_default(true)
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings");
+}
--- language-bindings/rust/crates/wamr-sys/build.rs
+++ language-bindings/rust/crates/wamr-sys/build.rs
@@ -3,38 +3,49 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
-extern crate bindgen;
-extern crate cmake;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
-use cmake::Config;
-use std::{env, path::Path, path::PathBuf};
+#[derive(Debug)]
+struct ParseLibPathError {
+    #[allow(dead_code)]
+    msg: String,
+}
 
-fn main() {
-    let wamr_root = Path::new("../../../../").canonicalize().unwrap();
-    assert!(wamr_root.exists());
+fn get_vmlib_path() -> Result<PathBuf, ParseLibPathError> {
+    let lib_path = env::var("WAMR_VMLIB_PATH").map_err(|e| ParseLibPathError {
+        msg: format!("'WAMR_VMLIB_PATH'. {}", e.to_string()),
+    })?;
 
-    let enable_llvm_jit = if cfg!(feature = "llvmjit") { "1" } else { "0" };
-    let dst = Config::new(&wamr_root)
-        // running mode
-        .define("WAMR_BUILD_AOT", "1")
-        .define("WAMR_BUILD_INTERP", "1")
-        .define("WAMR_BUILD_FAST_INTERP", "1")
-        .define("WAMR_BUILD_JIT", enable_llvm_jit)
-        // mvp
-        .define("WAMR_BUILD_BULK_MEMORY", "1")
-        .define("WAMR_BUILD_REF_TYPES", "1")
-        .define("WAMR_BUILD_SIMD", "1")
-        // wasi
-        .define("WAMR_BUILD_LIBC_WASI", "1")
-        // `nostdlib`
-        .define("WAMR_BUILD_LIBC_BUILTIN", "1")
-        .build_target("iwasm_static")
-        .build();
+    Path::new(&(lib_path))
+        .canonicalize()
+        .map_err(|e| ParseLibPathError {
+            msg: format!("'{}'. {}", lib_path, e.to_string()),
+        })
+        .map(|lib_path| lib_path)
+}
 
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+fn get_llvm_lib_path() -> Result<PathBuf, ParseLibPathError> {
+    let lib_path = env::var("WAMR_LLVMLIB_PATH").map_err(|e| ParseLibPathError {
+        msg: format!("'WAMR_LLVMLIB_PATH'. {}", e.to_string()),
+    })?;
+
+    Path::new(&(lib_path))
+        .canonicalize()
+        .map_err(|e| ParseLibPathError {
+            msg: format!("'{}'. {}", lib_path, e.to_string()),
+        })
+        .map(|lib_path| lib_path)
+}
+
+fn main() -> Result<(), ParseLibPathError> {
+    let vmlib_path = get_vmlib_path()?;
+
+    println!("cargo:rustc-link-search=native={}", vmlib_path.display());
     println!("cargo:rustc-link-lib=static=vmlib");
 
-    // //TODO: support macos?
     if cfg!(feature = "llvmjit") {
         println!("cargo:rustc-link-lib=dylib=dl");
         println!("cargo:rustc-link-lib=dylib=m");
@@ -42,97 +53,80 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
         println!("cargo:rustc-link-lib=dylib=z");
 
-        let llvm_dir = wamr_root.join("core/deps/llvm/build");
-        assert!(llvm_dir.exists());
-        println!("cargo:libdir={}/lib", llvm_dir.display());
-        println!("cargo:rustc-link-search=native={}/lib", llvm_dir.display());
+        let llvm_lib_path = get_llvm_lib_path()?;
+        println!("cargo:libdir={}", llvm_lib_path.display());
+        println!("cargo:rustc-link-search=native={}", llvm_lib_path.display());
 
-        println!("cargo:rustc-link-lib=static=LLVMXRay");
-        println!("cargo:rustc-link-lib=static=LLVMLibDriver");
-        println!("cargo:rustc-link-lib=static=LLVMDlltoolDriver");
-        println!("cargo:rustc-link-lib=static=LLVMCoverage");
-        println!("cargo:rustc-link-lib=static=LLVMLineEditor");
-        println!("cargo:rustc-link-lib=static=LLVMX86Disassembler");
-        println!("cargo:rustc-link-lib=static=LLVMX86AsmParser");
-        println!("cargo:rustc-link-lib=static=LLVMX86CodeGen");
-        println!("cargo:rustc-link-lib=static=LLVMX86Desc");
-        println!("cargo:rustc-link-lib=static=LLVMX86Info");
-        println!("cargo:rustc-link-lib=static=LLVMOrcJIT");
-        println!("cargo:rustc-link-lib=static=LLVMMCJIT");
-        println!("cargo:rustc-link-lib=static=LLVMJITLink");
-        println!("cargo:rustc-link-lib=static=LLVMInterpreter");
-        println!("cargo:rustc-link-lib=static=LLVMExecutionEngine");
-        println!("cargo:rustc-link-lib=static=LLVMRuntimeDyld");
-        println!("cargo:rustc-link-lib=static=LLVMOrcTargetProcess");
-        println!("cargo:rustc-link-lib=static=LLVMOrcShared");
-        println!("cargo:rustc-link-lib=static=LLVMDWP");
-        println!("cargo:rustc-link-lib=static=LLVMSymbolize");
-        println!("cargo:rustc-link-lib=static=LLVMDebugInfoPDB");
-        println!("cargo:rustc-link-lib=static=LLVMDebugInfoGSYM");
-        println!("cargo:rustc-link-lib=static=LLVMOption");
-        println!("cargo:rustc-link-lib=static=LLVMObjectYAML");
-        println!("cargo:rustc-link-lib=static=LLVMMCA");
-        println!("cargo:rustc-link-lib=static=LLVMMCDisassembler");
-        println!("cargo:rustc-link-lib=static=LLVMLTO");
-        println!("cargo:rustc-link-lib=static=LLVMPasses");
-        println!("cargo:rustc-link-lib=static=LLVMCFGuard");
-        println!("cargo:rustc-link-lib=static=LLVMCoroutines");
-        println!("cargo:rustc-link-lib=static=LLVMObjCARCOpts");
-        println!("cargo:rustc-link-lib=static=LLVMipo");
-        println!("cargo:rustc-link-lib=static=LLVMVectorize");
-        println!("cargo:rustc-link-lib=static=LLVMLinker");
-        println!("cargo:rustc-link-lib=static=LLVMInstrumentation");
-        println!("cargo:rustc-link-lib=static=LLVMFrontendOpenMP");
-        println!("cargo:rustc-link-lib=static=LLVMFrontendOpenACC");
-        println!("cargo:rustc-link-lib=static=LLVMExtensions");
-        println!("cargo:rustc-link-lib=static=LLVMDWARFLinker");
-        println!("cargo:rustc-link-lib=static=LLVMGlobalISel");
-        println!("cargo:rustc-link-lib=static=LLVMMIRParser");
-        println!("cargo:rustc-link-lib=static=LLVMAsmPrinter");
-        println!("cargo:rustc-link-lib=static=LLVMDebugInfoMSF");
-        println!("cargo:rustc-link-lib=static=LLVMDebugInfoDWARF");
-        println!("cargo:rustc-link-lib=static=LLVMSelectionDAG");
-        println!("cargo:rustc-link-lib=static=LLVMCodeGen");
-        println!("cargo:rustc-link-lib=static=LLVMIRReader");
-        println!("cargo:rustc-link-lib=static=LLVMAsmParser");
-        println!("cargo:rustc-link-lib=static=LLVMInterfaceStub");
-        println!("cargo:rustc-link-lib=static=LLVMFileCheck");
-        println!("cargo:rustc-link-lib=static=LLVMFuzzMutate");
-        println!("cargo:rustc-link-lib=static=LLVMTarget");
-        println!("cargo:rustc-link-lib=static=LLVMScalarOpts");
-        println!("cargo:rustc-link-lib=static=LLVMInstCombine");
-        println!("cargo:rustc-link-lib=static=LLVMAggressiveInstCombine");
-        println!("cargo:rustc-link-lib=static=LLVMTransformUtils");
-        println!("cargo:rustc-link-lib=static=LLVMBitWriter");
-        println!("cargo:rustc-link-lib=static=LLVMAnalysis");
-        println!("cargo:rustc-link-lib=static=LLVMProfileData");
-        println!("cargo:rustc-link-lib=static=LLVMObject");
-        println!("cargo:rustc-link-lib=static=LLVMTextAPI");
-        println!("cargo:rustc-link-lib=static=LLVMMCParser");
-        println!("cargo:rustc-link-lib=static=LLVMMC");
-        println!("cargo:rustc-link-lib=static=LLVMDebugInfoCodeView");
-        println!("cargo:rustc-link-lib=static=LLVMBitReader");
-        println!("cargo:rustc-link-lib=static=LLVMCore");
-        println!("cargo:rustc-link-lib=static=LLVMRemarks");
-        println!("cargo:rustc-link-lib=static=LLVMBitstreamReader");
-        println!("cargo:rustc-link-lib=static=LLVMBinaryFormat");
-        println!("cargo:rustc-link-lib=static=LLVMTableGen");
-        println!("cargo:rustc-link-lib=static=LLVMSupport");
-        println!("cargo:rustc-link-lib=static=LLVMDemangle");
+        for llvm_lib in &[
+            "LLVMAggressiveInstCombine",
+            "LLVMAnalysis",
+            "LLVMAsmParser",
+            "LLVMAsmPrinter",
+            "LLVMBitReader",
+            "LLVMBitWriter",
+            "LLVMCFGuard",
+            "LLVMCodeGen",
+            "LLVMCoroutines",
+            "LLVMCoverage",
+            "LLVMDWARFLinker",
+            "LLVMDWP",
+            "LLVMDebugInfoCodeView",
+            "LLVMDebugInfoDWARF",
+            "LLVMDebugInfoGSYM",
+            "LLVMDebugInfoMSF",
+            "LLVMDebugInfoPDB",
+            "LLVMDlltoolDriver",
+            "LLVMExecutionEngine",
+            "LLVMExtensions",
+            "LLVMFileCheck",
+            "LLVMFrontendOpenACC",
+            "LLVMFrontendOpenMP",
+            "LLVMFuzzMutate",
+            "LLVMGlobalISel",
+            "LLVMIRReader",
+            "LLVMInstCombine",
+            "LLVMInstrumentation",
+            "LLVMInterfaceStub",
+            "LLVMInterpreter",
+            "LLVMJITLink",
+            "LLVMLTO",
+            "LLVMLibDriver",
+            "LLVMLineEditor",
+            "LLVMLinker",
+            "LLVMMC",
+            "LLVMMCA",
+            "LLVMMCDisassembler",
+            "LLVMMCJIT",
+            "LLVMMCParser",
+            "LLVMMIRParser",
+            "LLVMObjCARCOpts",
+            "LLVMObject",
+            "LLVMObjectYAML",
+            "LLVMOption",
+            "LLVMOrcJIT",
+            "LLVMOrcShared",
+            "LLVMOrcTargetProcess",
+            "LLVMPasses",
+            "LLVMProfileData",
+            "LLVMRuntimeDyld",
+            "LLVMScalarOpts",
+            "LLVMSelectionDAG",
+            "LLVMSymbolize",
+            "LLVMTarget",
+            "LLVMTextAPI",
+            "LLVMTransformUtils",
+            "LLVMVectorize",
+            "LLVMX86AsmParser",
+            "LLVMX86CodeGen",
+            "LLVMX86Desc",
+            "LLVMX86Disassembler",
+            "LLVMX86Info",
+            "LLVMXRay",
+            "LLVMipo",
+        ] {
+            println!("cargo:rustc-link-lib=static={}", llvm_lib);
+        }
     }
 
-    let wamr_header = wamr_root.join("core/iwasm/include/wasm_export.h");
-    assert!(wamr_header.exists());
-
-    let bindings = bindgen::Builder::default()
-        .ctypes_prefix("::core::ffi")
-        .use_core()
-        .header(wamr_header.into_os_string().into_string().unwrap())
-        .derive_default(true)
-        .generate()
-        .expect("Unable to generate bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings");
+    Ok(())
 }
