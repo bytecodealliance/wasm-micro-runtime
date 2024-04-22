e0923a8cb94ddda699991a44a250cfbe1dce71da
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-04-22 12:40:49 +0900

    samples/native-stack-overflow/run.sh: add interpreter w/o hw bound check

--- samples/native-stack-overflow/run.sh
+++ samples/native-stack-overflow/run.sh
@@ -4,6 +4,10 @@ echo "====== Interpreter"
 out/native-stack-overflow out/wasm-apps/testapp.wasm
 
 echo
+echo "====== Interpreter WAMR_DISABLE_HW_BOUND_CHECK=1"
+out/native-stack-overflow.WAMR_DISABLE_HW_BOUND_CHECK out/wasm-apps/testapp.wasm
+
+echo
 echo "====== AOT"
 out/native-stack-overflow out/wasm-apps/testapp.wasm.aot
 
