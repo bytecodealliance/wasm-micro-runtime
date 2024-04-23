80ccf42f31a7a77d28b2cebe35f45a3e9270b68b
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-04-23 16:59:02 +0900

    samples/native-stack-overflow/run.sh: print test func name

--- samples/native-stack-overflow/run.sh
+++ samples/native-stack-overflow/run.sh
@@ -2,17 +2,17 @@
 
 NAME=${1:-test1}
 
-echo "====== Interpreter"
+echo "====== Interpreter ${NAME}"
 out/native-stack-overflow out/wasm-apps/testapp.wasm ${NAME}
 
 echo
-echo "====== Interpreter WAMR_DISABLE_HW_BOUND_CHECK=1"
+echo "====== Interpreter WAMR_DISABLE_HW_BOUND_CHECK=1 ${NAME}"
 out/native-stack-overflow.WAMR_DISABLE_HW_BOUND_CHECK out/wasm-apps/testapp.wasm ${NAME}
 
 echo
-echo "====== AOT"
+echo "====== AOT ${NAME}"
 out/native-stack-overflow out/wasm-apps/testapp.wasm.aot ${NAME}
 
 echo
-echo "====== AOT WAMR_DISABLE_HW_BOUND_CHECK=1"
+echo "====== AOT WAMR_DISABLE_HW_BOUND_CHECK=1 ${NAME}"
 out/native-stack-overflow.WAMR_DISABLE_HW_BOUND_CHECK out/wasm-apps/testapp.wasm.aot.bounds-checks ${NAME}
