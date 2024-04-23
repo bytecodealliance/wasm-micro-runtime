ea1d794a112de4bb244ae675171fef589230d1bd
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-04-23 18:17:31 +0900

    samples/native-stack-overflow/build.sh: fix shebang
    also switch to /bin/sh

--- samples/native-stack-overflow/build.sh
+++ samples/native-stack-overflow/build.sh
@@ -1,9 +1,9 @@
+#! /bin/sh
+
 #
 # Copyright (C) 2019 Intel Corporation.  All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-
-#!/bin/bash
 
 CURR_DIR=$PWD
 WAMR_DIR=${PWD}/../..
