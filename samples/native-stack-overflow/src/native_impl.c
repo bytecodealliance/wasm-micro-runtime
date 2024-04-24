a53ab7f323ab0cc12ebf8fbfdb10ca6c54bcea89
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-04-24 13:18:58 +0900

    samples/native-stack-overflow: improve memset_s detection and add fallback

--- samples/native-stack-overflow/src/native_impl.c
+++ samples/native-stack-overflow/src/native_impl.c
@@ -3,8 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
+#define __STDC_WANT_LIB_EXT1__ 1
+
 #include <inttypes.h>
 #include <stdio.h>
+#include <string.h>
+
+#if defined(__APPLE__)
+#include <Availability.h>
+#endif
 
 #include "wasm_export.h"
 #include "bh_platform.h"
@@ -61,8 +68,21 @@ consume_stack1(wasm_exec_env_t exec_env, void *base, uint32_t stack)
     ptrdiff_t diff = (unsigned char *)base - (unsigned char *)fp;
     assert(diff > 0);
     char buf[16];
-    /* note: unlike memset, memset_s is not allowed to be optimized away. */
+    /*
+     * note: we prefer to use memset_s here because, unlike memset,
+     * memset_s is not allowed to be optimized away.
+     *
+     * memset_s is available for macOS 10.13+ according to:
+     * https://developer.apple.com/documentation/kernel/2876438-memset_s
+     */
+#if defined(__STDC_LIB_EXT1__)                  \
+    || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) \
+        && __MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_3)
     memset_s(buf, sizeof(buf), 0, sizeof(buf));
+#else
+#warning memset_s is not available
+    memset(buf, 0, sizeof(buf));
+#endif
     if (diff > stack) {
         return diff;
     }
