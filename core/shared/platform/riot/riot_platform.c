4f3288ae0366447ac4f6d873286c554ff0670a95
mewen.berthelot <mewen.berthelot@orange.com>
2024-06-06 11:38:57 +0200

    Added missing functions to make RIOT work with the 2.x.x version

--- core/shared/platform/riot/riot_platform.c
+++ core/shared/platform/riot/riot_platform.c
@@ -57,6 +57,12 @@ os_mmap(void *hint, size_t size, int prot, int flags, os_file_handle file)
     return BH_MALLOC((unsigned)size);
 }
 
+void *
+os_mremap(void *old_addr, size_t old_size, size_t new_size)
+{
+    return os_mremap_slow(old_addr, old_size, new_size);
+}
+
 void
 os_munmap(void *addr, size_t size)
 {
