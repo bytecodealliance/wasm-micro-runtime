7931bfd950e85d858431bf69d061e05cb5891810
Zhang Yi <yi.y.zhang@intel.com>
2024-06-18 20:01:37 +0800

    Update license headers.

--- tests/standalone/standalone.sh
+++ tests/standalone/standalone.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
 
 # get every run.sh under standalone sub-direcotry
 #
