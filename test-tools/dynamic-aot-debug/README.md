56433bc77c60b42f5309161c5d6e9c9f6285c8e0
zhangliangyu3 <zhangliangyu3@xiaomi.com>
2024-09-11 23:28:47 +0800

    support dynamic aot debug
    Signed-off-by: zhangliangyu3 <zhangliangyu3@xiaomi.com>

---
+++ test-tools/dynamic-aot-debug/README.md
@@ -0,0 +1,55 @@
+# Dynamic AOT Module Debugging
+
+### How to use
+
+#### 1. Enable config
+
+```
+WASM_ENABLE_AOT=1
+WASM_ENABLE_DYNAMIC_AOT_DEBUG=1
+```
+
+#### 2. Build aot and obj file
+
+- build [wamrc](https://github.com/bytecodealliance/wasm-micro-runtime/tree/main/wamr-compiler) (enable WAMR_BUILD_DEBUG_AOT)
+- build test.c to test.wasm
+
+```
+/opt/wasi-sdk/bin/clang -O0 -nostdlib -z stack-size=8192 -Wl,--initial-memory=65536 -g -gdwarf-2   -o test.wasm test.c -Wl,--export=main -Wl,--export=__main_argc_argv  -Wl,--export=__heap_base -Wl,--export=__data_end -Wl,--no-entry -Wl,--allow-undefined
+```
+
+- build test.wasm to test.aot
+
+```
+./wamrc --opt-level=0 --target=thumbv7 --target-abi=gnueabihf--cpu=cortex-a7 --cpu-features=-neon -o test.aot test.wasm
+```
+
+- build test.wasm to test obj file
+
+```
+./wamrc --opt-level=0 --format=object --target=thumbv7 --target-abi=gnueabihf --cpu=cortex-a7 --cpu-features=-neon -o test test.wasm
+```
+
+#### 3. Start emulator and nuttx
+
+- Start emulator in terminal 1
+
+```
+/* start emulator on debug mode, and will start gdb server, set port as 1234 */
+./emulator.sh vela -qemu -S -s
+$ iwasm test.aot
+```
+
+- Start nuttx by GDB in terminal 2
+
+```
+/* You can save test obj file in this path */
+export OBJ_PATH=~/work/data/
+gdb-multiarch ./nuttx/nuttx -ex "tar remote:1234" -ex "source dynamic_aot_debug.py"
+$(gdb) c
+$(gdb) b main
+```
+
+#### 4. Workflow
+
+image:: dynamic_aot_debug_workflow.png
---
+++ test-tools/dynamic-aot-debug/dynamic_aot_debug.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+import os
+import gdb
+
+# get object file path from env，set default value "~/objects/"
+# exp: export OBJ_PATH=~/work/data/debug/
+path_objs = os.getenv("OBJ_PATH", "~/objects/")
+# us os.path.expand user expand user path symbol（such as ~）
+path_objs = os.path.expanduser(path_objs)
+print(f"Object files will be loaded from: {path_objs} on localhost")
+
+aot_module_info = {}
+
+
+def add_symbol_with_aot_info(aot_module_info):
+    """add symbol with aot info"""
+    text_addr = aot_module_info["code"]
+    file_name = aot_module_info["name"]
+
+    file_name_without_extension, file_extension = os.path.splitext(file_name)
+
+    if os.path.sep in file_name_without_extension:
+        file_name = os.path.basename(file_name_without_extension)
+    else:
+        file_name = file_name_without_extension
+
+    path_symfile = os.path.join(path_objs, file_name)
+
+    cmd = f"add-symbol-file {path_symfile} {text_addr}"
+    gdb.execute(cmd)
+
+    breakpoints = gdb.execute("info breakpoints", to_string=True)
+    print("Current breakpoints:", breakpoints)
+
+
+class read_g_dynamic_aot_module(gdb.Command):
+    """read_g_dynamic_aot_module"""
+
+    def __init__(self):
+        super(self.__class__, self).__init__("read_gda", gdb.COMMAND_USER)
+
+    def invoke(self, args, from_tty):
+        """invoke"""
+        Aot_module = gdb.parse_and_eval("g_dynamic_aot_module")
+        found_code = False
+        found_name = False
+        Aot_module_fields = []
+        if Aot_module.type.code == gdb.TYPE_CODE_PTR:
+            Aot_module = Aot_module.dereference()
+            if Aot_module.type.strip_typedefs().code == gdb.TYPE_CODE_STRUCT:
+                Aot_module_fields = [f.name for f in Aot_module.type.fields()]
+                for field in Aot_module_fields:
+                    var = Aot_module[field]
+                    if field == "name":
+                        aot_module_info["name"] = var.string()
+                        found_name = True
+                    elif field == "code":
+                        aot_module_info["code"] = var.address
+                        found_code = True
+                    if found_code == True and found_name == True:
+                        break
+            else:
+                print("Aot_module not struct type!")
+        else:
+            print("Aot_module not struct point type!")
+
+        add_symbol_with_aot_info(aot_module_info)
+
+
+def init():
+    """init"""
+    # register the command to gdb
+    read_g_dynamic_aot_module()
+    # set a breakpoint at function __enable_dynamic_aot_debug
+    breakpoint = gdb.Breakpoint("__enable_dynamic_aot_debug")
+    # attach the self-defined command to the created breakpoint
+    breakpoint.commands = "read_gda"
+
+
+init()
