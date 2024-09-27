bc36fd2e9056ce560f5507b3320bd52eb6a80eca
liang.he@intel.com <liang.he@intel.com>
2024-09-27 11:29:13 +0000

    Fix few issues found in tflite test

--- core/iwasm/libraries/wasi-nn/cmake/Findtensorflow_lite.cmake
+++ core/iwasm/libraries/wasi-nn/cmake/Findtensorflow_lite.cmake
@@ -4,20 +4,32 @@
 include(FetchContent)
 
 set(TFLITE_SOURCE_DIR "${WAMR_ROOT_DIR}/core/deps/tensorflow-src")
+if(EXISTS ${TFLITE_SOURCE_DIR})
+  message("Use existed source code under ${TFLITE_SOURCE_DIR}")
+  FetchContent_Declare(
+    tensorflow_lite
+    SOURCE_DIR     ${TFLITE_SOURCE_DIR}
+    SOURCE_SUBDIR  tensorflow/lite
+  )
+else()
+  message("download source code and store it at ${TFLITE_SOURCE_DIR}")
+  FetchContent_Declare(
+    tensorflow_lite
+    GIT_REPOSITORY https://github.com/tensorflow/tensorflow.git
+    GIT_TAG        v2.12.0
+    GIT_SHALLOW    ON
+    GIT_PROGRESS   ON
+    SOURCE_DIR     ${TFLITE_SOURCE_DIR}
+    SOURCE_SUBDIR  tensorflow/lite
+    PATCH_COMMAND  git apply ${CMAKE_CURRENT_LIST_DIR}/add_telemetry.patch
+  )
+endif()
 
-FetchContent_Declare(
-  tensorflow_lite
-  GIT_REPOSITORY https://github.com/tensorflow/tensorflow.git 
-  GIT_TAG        v2.12.0 
-  GIT_SHALLOW    ON
-  GIT_PROGRESS   ON
-  SOURCE_DIR     ${TFLITE_SOURCE_DIR}
-  SOURCE_SUBDIR  tensorflow/lite
-)
 
 if(WAMR_BUILD_WASI_NN_ENABLE_GPU EQUAL 1)
   set(TFLITE_ENABLE_GPU ON)
 endif()
+
 if (CMAKE_SIZEOF_VOID_P EQUAL 4)
   set(TFLITE_ENABLE_XNNPACK OFF)
 endif()
