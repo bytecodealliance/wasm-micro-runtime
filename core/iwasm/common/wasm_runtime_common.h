f74ef1f3dd063f0c9a729b86a039fcfa34efeea1
liang.he@intel.com <liang.he@intel.com>
2024-09-29 10:52:07 +0000

    Add APIs and test for instantiation linking
    including
    - memory create and destroy
    - instnace_new with imports

--- core/iwasm/common/wasm_runtime_common.h
+++ core/iwasm/common/wasm_runtime_common.h
@@ -575,6 +575,8 @@ wasm_runtime_instantiate_internal(WASMModuleCommon *module,
                                   WASMModuleInstanceCommon *parent,
                                   WASMExecEnv *exec_env_main, uint32 stack_size,
                                   uint32 heap_size, uint32 max_memory_pages,
+                                  uint32 import_count,
+                                  const struct WasmExternalInstance *imports,
                                   char *error_buf, uint32 error_buf_size);
 
 /* Internal API */
