8397c6eb7afe07500d92f8eb57f0e841a283491b
liang.he@intel.com <liang.he@intel.com>
2024-10-09 05:23:24 +0000

    Refactor CMakeLists.txt and import_memory.c
    - Set CMAKE_BUILD_TYPE to Debug in CMakeLists.txt
    - Add missing printf and fflush statements in import_memory.c
    - Add assertion for memory data in import_memory.c
    - Add assertion for memory growth in import_memory.c

--- samples/linking/wasm/import_memory.c
+++ samples/linking/wasm/import_memory.c
@@ -31,15 +31,16 @@ __attribute__((export_name("goodhart_law"))) void
 goodhart_law()
 {
     struct Buffer *buffer = new_buffer(64);
-    // not 0xEE as pre-allocated buffer
-    assert(buffer->data[30] == 0);
+    assert(buffer->data != NULL);
 
     snprintf(buffer->data, 60, "%s",
              "When a measure becomes a target, it ceases to be a good measure");
-    fprintf(stderr, "%s\n", buffer->data);
     assert(buffer->data[10] == 's');
 
     release_buffer(buffer);
+
+    printf("-> pass basic\n");
+    fflush(stdout);
 
     // alloc a huge buffer
     struct Buffer *huge_buffer = new_buffer(60000);
@@ -50,27 +51,15 @@ goodhart_law()
 
     release_buffer(huge_buffer);
 
-    // grow 1 page
-    bool grow = false;
-    for (unsigned i = 0; i < 65535; i++) {
-        int buffer_size = 65535 + i * 1000;
-        struct Buffer *buffer = new_buffer(buffer_size);
-        if (buffer->data == NULL) {
-            if (!grow) {
-                fprintf(stderr, "Failed to allocate buffer at %u. Let's grow\n",
-                        buffer_size);
-                grow = true;
-                int32_t ret = __builtin_wasm_memory_grow(0, 1);
-                assert(ret > 0);
-            }
-            else {
-                break;
-            }
-        }
-        else {
-            release_buffer(buffer);
-        }
-    }
+    printf("-> pass huge allocations\n");
+    fflush(stdout);
+
+    int ret = __builtin_wasm_memory_grow(0, 1);
+    // sync with the value assigned by `--initial-memory`
+    assert(ret == 3);
+
+    printf("-> pass manually memory.grow\n");
+    fflush(stdout);
 }
 
 int
