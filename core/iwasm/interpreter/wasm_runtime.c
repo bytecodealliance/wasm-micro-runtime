0ef37c22950f52dc0cf0915809b751ec24278e2e
James Ring <sjr@jdns.org>
2024-11-21 20:05:25 -0800

    Use plain assignment rather than bh_memcpy_s

--- core/iwasm/interpreter/wasm_runtime.c
+++ core/iwasm/interpreter/wasm_runtime.c
@@ -1208,9 +1208,8 @@ globals_instantiate(WASMModule *module, WASMModuleInstance *module_inst,
 
             /* The linked global instance has been initialized, we
                just need to copy the value. */
-            bh_memcpy_s(&(global->initial_value), sizeof(WASMValue),
-                        &(global_import->import_global_linked->init_expr.u),
-                        sizeof(WASMValue));
+            global->initial_value =
+                global_import->import_global_linked->init_expr.u;
         }
         else
 #endif
