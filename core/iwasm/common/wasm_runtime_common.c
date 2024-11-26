be317cee815b3c32fedcfb960bcadeee2ddf6a15
liang.he@intel.com <liang.he@intel.com>
2024-11-26 11:45:19 +0000

    Refactor: conditionally compile extern instance destruction for spec and WASI tests

--- core/iwasm/common/wasm_runtime_common.c
+++ core/iwasm/common/wasm_runtime_common.c
@@ -8116,6 +8116,7 @@ wasm_runtime_destroy_extern_inst(WASMModuleCommon *module,
     extern_inst->field_name = NULL;
 }
 
+#if WASM_ENABLE_SPEC_TEST != 0 || WASM_ENABLE_WASI_TEST != 0
 /*
  * Be aware that it will remove all items in the list, regardless of whether
  * they were created by the runtime (for built-ins) or by users.
@@ -8132,6 +8133,7 @@ wasm_runtime_destroy_imports(WASMModuleCommon *module,
         wasm_runtime_destroy_extern_inst(module, extern_inst_list + i);
     }
 }
+#endif /* WASM_ENABLE_SPEC_TEST != 0 || WASM_ENABLE_WASI_TEST != 0 */
 
 bool
 wasm_runtime_create_imports_with_builtin(WASMModuleCommon *module,
