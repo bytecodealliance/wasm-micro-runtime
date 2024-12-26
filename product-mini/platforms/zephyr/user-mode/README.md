edd03948aaea718ac0b9fa5141887b01ae3664dc
TL <tianlong.liang@intel.com>
2024-12-26 12:24:55 +0800

    first draft of how to embed wamr in zephyr user mode

---
+++ product-mini/platforms/zephyr/user-mode/README.md
@@ -0,0 +1,3 @@
+# User mode example
+
+This example demonstrates how to build and run a WebAssembly application in user mode on Zephyr. 
---
+++ product-mini/platforms/zephyr/user-mode/src/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/app_memory/app_memdomain.h>
+
+#define MAIN_THREAD_STACK_SIZE 2048
+#define MAIN_THREAD_PRIORITY 5
+
+static struct k_thread iwasm_user_mode_thread;
+K_THREAD_STACK_DEFINE(iwasm_user_mode_thread_stack, MAIN_THREAD_STACK_SIZE);
+
+extern struct k_mem_partition z_libc_partition;
+K_APPMEM_PARTITION_DEFINE(wamr_partition);
+
+/* WAMR memory domain */
+struct k_mem_domain wamr_domain;
+
+extern void
+iwasm_main(void *arg1, void *arg2, void *arg3);
+
+bool
+iwasm_user_mode(void)
+{
+    struct k_mem_partition *wamr_domain_parts[] = { &wamr_partition,
+                                                    &z_libc_partition };
+
+    printk("Native: wamr_partition start addr: %p, size: %zu\n", wamr_partition.start,
+           wamr_partition.size);
+
+    /* Initialize the memory domain with single WAMR partition */
+    k_mem_domain_init(&wamr_domain, 2, wamr_domain_parts);
+
+    printk("Native: finish domain init\n");
+
+    k_tid_t tid =
+        k_thread_create(&iwasm_user_mode_thread, iwasm_user_mode_thread_stack,
+                        MAIN_THREAD_STACK_SIZE, iwasm_main, NULL, NULL, NULL,
+                        MAIN_THREAD_PRIORITY, K_USER, K_NO_WAIT);
+
+    printk("Native: add domain to thread start\n");
+    /* Grant WAMR memory domain access to user mode thread */
+    k_mem_domain_add_thread(&wamr_domain, tid);
+    // printk("add domain to thread %p finish\n", tid);
+    k_thread_start(&tid);
+
+    return tid ? true : false;
+}
+
+#if KERNEL_VERSION_NUMBER < 0x030400 /* version 3.4.0 */
+void
+main(void)
+{
+    iwasm_user_mode();
+    printk("Native: Hello, world from main thread\n");
+}
+#else
+int
+main(void)
+{
+    iwasm_user_mode();
+    return 0;
+}
+#endif
