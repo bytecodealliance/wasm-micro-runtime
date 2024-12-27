11b49156c6b4d40c850df461fc63b554f4e38cdd
TL <tianlong.liang@intel.com>
2024-12-27 12:12:19 +0800

    add license header

--- product-mini/platforms/zephyr/user-mode/src/main.c
+++ product-mini/platforms/zephyr/user-mode/src/main.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 #include <stdio.h>
 
 #include <zephyr/app_memory/app_memdomain.h>
