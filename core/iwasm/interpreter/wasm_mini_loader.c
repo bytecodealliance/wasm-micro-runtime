b6dea221a609606ce6c3dcaa0f4d0399ea5bc4dc
TianlongLiang <111852609+TianlongLiang@users.noreply.github.com>
2025-01-21 13:08:09 +0800

    Fix wasm loader check data segment count (#4039)
    correctly report error when datacount section has non-zero data segment count while the data section is not present

--- core/iwasm/interpreter/wasm_mini_loader.c
+++ core/iwasm/interpreter/wasm_mini_loader.c
@@ -2734,6 +2734,11 @@ load_from_sections(WASMModule *module, WASMSection *sections,
         section = section->next;
     }
 
+#if WASM_ENABLE_BULK_MEMORY != 0
+    bh_assert(!has_datacount_section
+              || module->data_seg_count == module->data_seg_count1);
+#endif
+
     module->aux_data_end_global_index = (uint32)-1;
     module->aux_heap_base_global_index = (uint32)-1;
     module->aux_stack_top_global_index = (uint32)-1;
