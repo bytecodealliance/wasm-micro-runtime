6bfc08849a8bc8c2031442aef625e12b863a5bf5
Georgii Rylov <godjan@amazon.co.uk>
2025-01-28 11:09:32 +0000

    Calculate func_index instead of adding an extra field to wasm frame

--- core/iwasm/interpreter/wasm_interp_fast.c
+++ core/iwasm/interpreter/wasm_interp_fast.c
@@ -1205,11 +1205,9 @@ wasm_interp_call_func_native(WASMModuleInstance *module_inst,
     init_frame_refs(frame->frame_ref, local_cell_num, cur_func);
 #endif
 
-    cur_func_index = (uint32)(cur_func - module_inst->e->functions);
-    frame->func_index = cur_func_index;
-
     wasm_exec_env_set_cur_frame(exec_env, frame);
 
+    cur_func_index = (uint32)(cur_func - module_inst->e->functions);
     bh_assert(cur_func_index < module_inst->module->import_function_count);
     if (!func_import->call_conv_wasm_c_api) {
         native_func_pointer = module_inst->import_func_ptrs[cur_func_index];
@@ -6034,7 +6032,6 @@ wasm_interp_call_func_bytecode(WASMModuleInstance *module,
 
             /* Initialize the interpreter context. */
             frame->function = cur_func;
-            frame->func_index = (uint32)(cur_func - module->e->functions);
             frame_ip = wasm_get_func_code(cur_func);
             frame_ip_end = wasm_get_func_code_end(cur_func);
 
