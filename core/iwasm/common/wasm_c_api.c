de82d1946f8a215e529fca04268d096051a2b313
Jesse Wilson <jwilson@squareup.com>
2025-03-03 20:45:59 -0500

    Address code review feedback

--- core/iwasm/common/wasm_c_api.c
+++ core/iwasm/common/wasm_c_api.c
@@ -392,8 +392,8 @@ wasm_engine_new_internal(wasm_config_t *config)
 
     WASM_C_DUMP_PROC_MEM();
 
-    /* wasm_config_t->MemAllocOption -> RuntimeInitArgs->MemAllocOption */
     init_args.gc_heap_size = GC_HEAP_SIZE_DEFAULT;
+    /* wasm_config_t->MemAllocOption -> RuntimeInitArgs->MemAllocOption */
     init_args.mem_alloc_type = config->mem_alloc_type;
     memcpy(&init_args.mem_alloc_option, &config->mem_alloc_option,
            sizeof(MemAllocOption));
