9458e9646640585b854aef6593f8bdacb87d21e5
zhanheng1 <Zhanheng.Qin@sony.com>
2025-03-28 10:35:36 +0800

    Remove xtensa constraints for f64_cmp to avoid relocations

--- core/iwasm/compilation/aot_emit_compare.c
+++ core/iwasm/compilation/aot_emit_compare.c
@@ -205,7 +205,7 @@ aot_compile_op_f64_compare(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx,
 
     if (comp_ctx->disable_llvm_intrinsics
         && aot_intrinsic_check_capability(comp_ctx, "f64_cmp")
-        && strcmp(comp_ctx->target_arch, "xtensa")) {
+        ) {
         LLVMTypeRef param_types[3];
         LLVMValueRef opcond = LLVMConstInt(I32_TYPE, cond, true);
         param_types[0] = I32_TYPE;
