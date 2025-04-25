f960e14033e41606ad4756151df5e19398afd550
TL <tianlong.liang@intel.com>
2025-04-25 12:10:17 +0800

    add more unit test for shared heap

--- tests/unit/shared-heap/shared_heap_test.cc
+++ tests/unit/shared-heap/shared_heap_test.cc
@@ -203,18 +203,15 @@ TEST_F(shared_heap_test, test_preallocated_shared_heap_malloc_fail)
     EXPECT_EQ(0, argv[0]);
 }
 
-TEST_F(shared_heap_test, test_shared_heap_chain_rmw)
+static void
+create_test_shared_heap_chain(uint8 *preallocated_buf, size_t size,
+                              uint8 *preallocated_buf2, size_t size2,
+                              WASMSharedHeap **shared_heap_chain)
 {
     SharedHeapInitArgs args = { 0 };
-    WASMSharedHeap *shared_heap = nullptr, *shared_heap2 = nullptr,
-                   *shared_heap_chain = nullptr;
-    uint32 argv[2] = { 0 }, BUF_SIZE = os_getpagesize();
-    uint8 preallocated_buf[BUF_SIZE] = { 0 },
-          preallocated_buf2[BUF_SIZE] = { 0 };
-    uint32 start1, end1, start2, end2;
-
+    WASMSharedHeap *shared_heap = nullptr, *shared_heap2 = nullptr;
     args.pre_allocated_addr = preallocated_buf;
-    args.size = BUF_SIZE;
+    args.size = size;
     shared_heap = wasm_runtime_create_shared_heap(&args);
     if (!shared_heap) {
         FAIL() << "Create preallocated shared heap failed.\n";
@@ -222,23 +219,36 @@ TEST_F(shared_heap_test, test_shared_heap_chain_rmw)
 
     memset(&args, 0, sizeof(args));
     args.pre_allocated_addr = preallocated_buf2;
-    args.size = BUF_SIZE;
+    args.size = size2;
     shared_heap2 = wasm_runtime_create_shared_heap(&args);
     if (!shared_heap2) {
         FAIL() << "Create preallocated shared heap failed.\n";
     }
 
-    shared_heap_chain =
+    *shared_heap_chain =
         wasm_runtime_chain_shared_heaps(shared_heap, shared_heap2);
-    if (!shared_heap_chain) {
+    if (!*shared_heap_chain) {
         FAIL() << "Create shared heap chain failed.\n";
     }
+}
+
+TEST_F(shared_heap_test, test_shared_heap_chain_rmw)
+{
+    SharedHeapInitArgs args = { 0 };
+    WASMSharedHeap *shared_heap_chain = nullptr;
+    uint32 argv[2] = { 0 }, BUF_SIZE = os_getpagesize();
+    uint8 preallocated_buf[BUF_SIZE] = { 0 },
+          preallocated_buf2[BUF_SIZE] = { 0 };
+    uint32 start1, end1, start2, end2;
+
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
 
     /* app addr for shared heap */
-    start1 = 0xFFFFFFFF - 2 * BUF_SIZE + 1;
-    end1 = 0xFFFFFFFF - BUF_SIZE;
-    start2 = 0xFFFFFFFF - BUF_SIZE + 1;
-    end2 = 0xFFFFFFFF;
+    start1 = UINT32_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT32_MAX - BUF_SIZE;
+    start2 = UINT32_MAX - BUF_SIZE + 1;
+    end2 = UINT32_MAX;
 
     /* shared heap 1 */
     argv[0] = end1;
@@ -271,45 +281,99 @@ TEST_F(shared_heap_test, test_shared_heap_chain_rmw)
     EXPECT_EQ(preallocated_buf2[BUF_SIZE - 1], 81);
 }
 
-TEST_F(shared_heap_test, test_shared_heap_chain_rmw_oob)
+TEST_F(shared_heap_test, test_shared_heap_chain_rmw_bulk_memory)
 {
     SharedHeapInitArgs args = { 0 };
-    WASMSharedHeap *shared_heap = nullptr, *shared_heap2 = nullptr,
-                   *shared_heap_chain = nullptr;
+    WASMSharedHeap *shared_heap_chain = nullptr;
+    uint32 argv[3] = { 0 }, BUF_SIZE = os_getpagesize();
+    uint8 preallocated_buf[BUF_SIZE] = { 0 },
+          preallocated_buf2[BUF_SIZE] = { 0 };
+    uint32 start1, end1, start2, end2;
+
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
+
+    /* app addr for shared heap */
+    start1 = UINT32_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT32_MAX - BUF_SIZE;
+    start2 = UINT32_MAX - BUF_SIZE + 1;
+    end2 = UINT32_MAX;
+
+    /* shared heap 1 */
+    argv[0] = end1;
+    argv[1] = 101;
+    argv[2] = 1;
+    test_shared_heap(shared_heap_chain, "test_bulk_memory.wasm",
+                     "memory_fill_test", 3, argv);
+    /* no modification since no return value */
+    EXPECT_EQ(end1, argv[0]);
+    EXPECT_EQ(preallocated_buf[BUF_SIZE - 1], 101);
+
+    argv[0] = start2;
+    argv[1] = 98;
+    argv[2] = 2;
+    test_shared_heap(shared_heap_chain, "test_bulk_memory.aot",
+                     "memory_fill_test", 3, argv);
+    /* no modification since no return value */
+    EXPECT_EQ(start2, argv[0]);
+    EXPECT_EQ(preallocated_buf2[0], 98);
+}
+
+TEST_F(shared_heap_test, test_shared_heap_chain_rmw_bulk_memory_oob)
+{
+    SharedHeapInitArgs args = { 0 };
+    WASMSharedHeap *shared_heap_chain = nullptr;
+    uint32 argv[3] = { 0 }, BUF_SIZE = os_getpagesize();
+    uint8 preallocated_buf[BUF_SIZE] = { 0 },
+          preallocated_buf2[BUF_SIZE] = { 0 };
+    uint32 start1, end1, start2, end2;
+
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
+
+    /* app addr for shared heap */
+    start1 = UINT32_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT32_MAX - BUF_SIZE;
+    start2 = UINT32_MAX - BUF_SIZE + 1;
+    end2 = UINT32_MAX;
+
+    /* shared heap 1 */
+    argv[0] = end2;
+    argv[1] = 101;
+    argv[2] = 2;
+    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain,
+                                             "test_bulk_memory.wasm",
+                                             "memory_fill_test", 3, argv),
+                            "Exception: out of bounds memory access");
+
+    argv[0] = end1;
+    argv[1] = 98;
+    argv[2] = 2;
+    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain,
+                                             "test_bulk_memory.aot",
+                                             "memory_fill_test", 3, argv),
+                            "Exception: out of bounds memory access");
+}
+
+TEST_F(shared_heap_test, test_shared_heap_chain_rmw_oob)
+{
+    WASMSharedHeap *shared_heap_chain = nullptr;
     uint32 argv[2] = { 0 }, BUF_SIZE = os_getpagesize();
     uint8 preallocated_buf[BUF_SIZE], preallocated_buf2[BUF_SIZE];
     uint32 start1, end1, start2, end2;
 
-    args.pre_allocated_addr = preallocated_buf;
-    args.size = BUF_SIZE;
-    shared_heap = wasm_runtime_create_shared_heap(&args);
-    if (!shared_heap) {
-        FAIL() << "Create preallocated shared heap failed.\n";
-    }
-
-    memset(&args, 0, sizeof(args));
-    args.pre_allocated_addr = preallocated_buf2;
-    args.size = BUF_SIZE;
-    shared_heap2 = wasm_runtime_create_shared_heap(&args);
-    if (!shared_heap2) {
-        FAIL() << "Create preallocated shared heap failed.\n";
-    }
-
-    shared_heap_chain =
-        wasm_runtime_chain_shared_heaps(shared_heap, shared_heap2);
-    if (!shared_heap_chain) {
-        FAIL() << "Create shared heap chain failed.\n";
-    }
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
 
     /* app addr for shared heap */
-    start1 = 0xFFFFFFFF - 2 * BUF_SIZE + 1;
-    end1 = 0xFFFFFFFF - BUF_SIZE;
-    start2 = 0xFFFFFFFF - BUF_SIZE + 1;
-    end2 = 0xFFFFFFFF;
+    start1 = UINT32_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT32_MAX - BUF_SIZE;
+    start2 = UINT32_MAX - BUF_SIZE + 1;
+    end2 = UINT32_MAX;
 
     /* try to rmw an u16, first u8 is in the first shared heap and second u8 is
      * in the second shared heap, will be seen as oob */
-    argv[0] = end1;
+    argv[0] = end2;
     argv[1] = 12025;
     EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain, "test.wasm",
                                              "read_modify_write_16", 2, argv),
@@ -317,10 +381,91 @@ TEST_F(shared_heap_test, test_shared_heap_chain_rmw_oob)
 
     argv[0] = end1;
     argv[1] = 12025;
-    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain, "test.wasm",
+    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain, "test.aot",
                                              "read_modify_write_16", 2, argv),
                             "Exception: out of bounds memory access");
 }
+
+#if WASM_ENABLE_MEMORY64 != 0
+TEST_F(shared_heap_test, test_shared_heap_chain_memory64_rmw)
+{
+    WASMSharedHeap *shared_heap_chain = nullptr;
+    uint32 argv[3] = { 0 }, BUF_SIZE = os_getpagesize();
+    uint8 preallocated_buf[BUF_SIZE] = { 0 },
+          preallocated_buf2[BUF_SIZE] = { 0 };
+    uint64 start1, end1, start2, end2;
+
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
+
+    /* app addr for shared heap */
+    start1 = UINT64_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT64_MAX - BUF_SIZE;
+    start2 = UINT64_MAX - BUF_SIZE + 1;
+    end2 = UINT64_MAX;
+
+    /* shared heap 1 */
+    PUT_I64_TO_ADDR(argv, end1);
+    argv[2] = 101;
+    test_shared_heap(shared_heap_chain, "test64.wasm", "read_modify_write_8", 3,
+                     argv);
+    EXPECT_EQ(0, argv[0]);
+    EXPECT_EQ(preallocated_buf[BUF_SIZE - 1], 101);
+
+    /* shared heap 2 */
+    PUT_I64_TO_ADDR(argv, start2);
+    argv[2] = 129;
+    test_shared_heap(shared_heap_chain, "test64.wasm", "read_modify_write_8", 3,
+                     argv);
+    EXPECT_EQ(0, argv[0]);
+    EXPECT_EQ(preallocated_buf2[0], 129);
+
+    PUT_I64_TO_ADDR(argv, start1);
+    argv[2] = 98;
+    test_shared_heap(shared_heap_chain, "test64.aot", "read_modify_write_8", 3,
+                     argv);
+    EXPECT_EQ(0, argv[0]);
+    EXPECT_EQ(preallocated_buf[0], 98);
+
+    PUT_I64_TO_ADDR(argv, end2);
+    argv[2] = 81;
+    test_shared_heap(shared_heap_chain, "test64.aot", "read_modify_write_8", 3,
+                     argv);
+    EXPECT_EQ(0, argv[0]);
+    EXPECT_EQ(preallocated_buf2[BUF_SIZE - 1], 81);
+}
+
+TEST_F(shared_heap_test, test_shared_heap_chain_memory64_rmw_oob)
+{
+    WASMSharedHeap *shared_heap_chain = nullptr;
+    uint32 argv[3] = { 0 }, BUF_SIZE = os_getpagesize();
+    uint8 preallocated_buf[BUF_SIZE], preallocated_buf2[BUF_SIZE];
+    uint64 start1, end1, start2, end2;
+
+    create_test_shared_heap_chain(preallocated_buf, BUF_SIZE, preallocated_buf2,
+                                  BUF_SIZE, &shared_heap_chain);
+
+    /* app addr for shared heap */
+    start1 = UINT64_MAX - 2 * BUF_SIZE + 1;
+    end1 = UINT64_MAX - BUF_SIZE;
+    start2 = UINT64_MAX - BUF_SIZE + 1;
+    end2 = UINT64_MAX;
+
+    /* try to rmw an u16, first u8 is in the first shared heap and second u8 is
+     * in the second shared heap, will be seen as oob */
+    PUT_I64_TO_ADDR(argv, end1);
+    argv[2] = 12025;
+    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain, "test64.wasm",
+                                             "read_modify_write_16", 3, argv),
+                            "Exception: out of bounds memory access");
+
+    PUT_I64_TO_ADDR(argv, end1);
+    argv[2] = 12025;
+    EXPECT_NONFATAL_FAILURE(test_shared_heap(shared_heap_chain, "test64.aot",
+                                             "read_modify_write_16", 3, argv),
+                            "Exception: out of bounds memory access");
+}
+#endif
 
 #ifndef native_function
 /* clang-format off */
