501729608b412b5a99a2aee608e9a0c2bb11aeaf
YAMAMOTO Takashi <yamamoto@midokura.com>
2025-06-05 11:30:48 +0900

    wasi_ephemeral_nn.h: add a convenience wrapper header

---
+++ core/iwasm/libraries/wasi-nn/include/wasi_ephemeral_nn.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2025 Midokura Japan KK.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#define WASM_ENABLE_WASI_EPHEMERAL_NN 1
+#include "wasi_nn.h"
