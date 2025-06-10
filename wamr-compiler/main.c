1c1066dbb6fd66fb08fd35dde81c3a946fc10eb9
TL <tianlong.liang@intel.com>
2025-06-10 17:24:39 +0800

    enable aot memory64 sw bounds checks by default

--- wamr-compiler/main.c
+++ wamr-compiler/main.c
@@ -137,9 +137,10 @@ print_help()
     printf("                              3 - Small code model\n");
     printf("  -sgx                      Generate code for SGX platform (Intel Software Guard Extensions)\n");
     printf("  --bounds-checks=1/0       Enable or disable the bounds checks for memory access:\n");
-    printf("                              by default it is disabled in all 64-bit platforms except SGX and\n");
-    printf("                              in these platforms runtime does bounds checks with hardware trap,\n");
-    printf("                              and by default it is enabled in all 32-bit platforms\n");
+    printf("                              By default, it is disabled on all 64-bit platforms except SGX or when\n");
+    printf("                              memory64 is enabled. In these platform and feature combinations,\n");
+    printf("                              the runtime performs bounds checks with a hardware trap;\n");
+    printf("                              By default, it is enabled on all 32-bit platforms or when using memory64.\n");
     printf("                            CAVEAT: --bounds-checks=0 enables some optimizations\n");
     printf("                              which make the compiled AOT module incompatible\n");
     printf("                              with a runtime without the hardware bounds checks.\n");
