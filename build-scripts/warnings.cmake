c000ba9827d40f3bbf7a5f8ba7b29f4a0714f9ee
liang.he@intel.com <liang.he@intel.com>
2025-06-20 06:51:15 +0000

    Turn warnings into errors in CI
    - Apply global warnings in warnings.cmake instead of maintaining them in
      separate files.
    - Enable errors during CI when building iwasm and wamrc.
    - Since GCC and Clang are the default compilers on Ubuntu and macOS, enabling
      `-Werror` on both platforms can be treated as checking with different
      compilers.

---
+++ build-scripts/warnings.cmake
@@ -0,0 +1,28 @@
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# global additional warnings.
+if (MSVC)
+  # warning level 4
+  add_compile_options(/W4)
+else ()
+  # refer to https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
+  add_compile_options(
+    -Wall -Wextra -Wformat -Wformat-security
+     $<$<COMPILE_LANGUAGE:C>:-Wshadow>
+  )
+  # -pedantic causes warnings like "ISO C forbids initialization between function pointer and ‘void *’" which
+  #   is widely used in the codebase.
+  #
+  # -fpermissive causes warnings like "-fpermissive is valid for C++/ObjC++ but not for C"
+  #
+  add_compile_options (
+    $<$<COMPILE_LANGUAGE:C>:-Wincompatible-pointer-types>
+    $<$<COMPILE_LANGUAGE:C>:-Wimplicit-function-declaration>
+  )
+  # waivers
+  add_compile_options (
+    -Wno-unused
+    -Wno-unused-parameter
+  )
+endif ()
