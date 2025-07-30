2685f2cae0141361a780fbf7826a00b4d6284cc8
Marcin Kolny <mkolny@amazon.com>
2025-07-30 04:25:52 +0200

    Use venv to install llvm dependencies and run build script (#4514)
    Installing pip packages system-wide is no longer recommended, therefore
    we create virtual environment for setting up the build environment for LLVM.

--- wamr-compiler/build_llvm.sh
+++ wamr-compiler/build_llvm.sh
@@ -3,5 +3,17 @@
 # Copyright (C) 2020 Intel Corporation. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-/usr/bin/env python3 -m pip install --user -r ../build-scripts/requirements.txt
+TEMP_DIR=$(mktemp -d)
+
+cleanup() {
+    local exit_code=$?
+    rm -rf "$TEMP_DIR"
+    exit $exit_code
+}
+
+trap cleanup EXIT INT TERM
+
+/usr/bin/env python3 -m venv --clear "$TEMP_DIR"
+source "$TEMP_DIR/bin/activate"
+/usr/bin/env python3 -m pip install -r ../build-scripts/requirements.txt
 /usr/bin/env python3 ../build-scripts/build_llvm.py "$@"
