33eff933c0102fedb675dbd5173879bbcb8fe639
Zhenwei Jin <109658203+kylo5aby@users.noreply.github.com>
2025-08-01 08:45:57 +0800

    add validation for rec indices in aot loader (#4520)

--- core/iwasm/aot/aot_loader.c
+++ core/iwasm/aot/aot_loader.c
@@ -1805,7 +1805,12 @@ load_types(const uint8 **p_buf, const uint8 *buf_end, AOTModule *module,
         read_uint32(buf, buf_end, parent_type_idx);
         read_uint16(buf, buf_end, rec_count);
         read_uint16(buf, buf_end, rec_idx);
-
+#if WASM_ENABLE_AOT_VALIDATOR != 0
+        if (rec_idx > i) {
+            set_error_buf(error_buf, error_buf_size, "invalid rec_idx");
+            goto fail;
+        }
+#endif
         if (type_flag == WASM_TYPE_FUNC) {
             AOTFuncType *func_type;
 
