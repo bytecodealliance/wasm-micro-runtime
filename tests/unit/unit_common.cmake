180b642a6b75d96d4d726027f830ff67f0e5aed5
liang.he@intel.com <liang.he@intel.com>
2025-09-07 13:49:49 +0000

    refactor: streamline CMake configurations and improve code coverage collection
    - only unit/CMakeLists.txt fetches googletest
    - only unit/unit_common.cmake find LLVM dependencies
    - use `WAMR_RUNTIME_LIB_SOURCE` to replace a long list

--- tests/unit/unit_common.cmake
+++ tests/unit/unit_common.cmake
@@ -1,13 +1,6 @@
 # Copyright (C) 2019 Intel Corporation.  All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Yes. To solve the compatibility issue with CMAKE (>= 4.0), we need to update
-# our `cmake_minimum_required()` to 3.5. However, there are CMakeLists.txt
-# from 3rd parties that we should not alter. Therefore, in addition to
-# changing the `cmake_minimum_required()`, we should also add a configuration
-# here that is compatible with earlier versions.
-set(CMAKE_POLICY_VERSION_MINIMUM 3.5 FORCE)
-
 if (NOT DEFINED WAMR_BUILD_PLATFORM)
   set (WAMR_BUILD_PLATFORM "linux")
 endif ()
@@ -17,10 +10,6 @@ set (UNIT_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR})
 include_directories(${UNIT_ROOT_DIR})
 
 enable_language (ASM)
-
-# Reset default linker flags
-set (CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
-set (CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 
 # Set WAMR_BUILD_TARGET, currently values supported:
 # "X86_64", "AMD_64", "X86_32", "ARM_32", "MIPS_32", "XTENSA_32"
@@ -71,11 +60,6 @@ if (NOT DEFINED WAMR_BUILD_APP_FRAMEWORK)
   set (WAMR_BUILD_APP_FRAMEWORK 1)
 endif ()
 
-if (COLLECT_CODE_COVERAGE EQUAL 1)
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fprofile-arcs -ftest-coverage")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage")
-endif ()
-
 set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
 set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99 -ffunction-sections -fdata-sections \
                                      -Wall -Wno-unused-parameter -Wno-pedantic")
@@ -90,23 +74,20 @@ include_directories (${SHARED_DIR}/include
 
 include (${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
 
-if (NOT (GOOGLETEST_INCLUDED EQUAL 1))
-# Prevent overriding the parent project's compiler/linker
-# settings on Windows
-set (gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-# Fetch Google test
-include (FetchContent)
-FetchContent_Declare (
-    googletest
-    URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
-)
-FetchContent_MakeAvailable (googletest)
-
-endif()
-
 # Add helper classes
 include_directories(${CMAKE_CURRENT_LIST_DIR}/common)
 
-message ("unit_common.cmake included")
+# Involve LLVM for AOT and JIT
+set(LLVM_SRC_ROOT "${WAMR_ROOT_DIR}/core/deps/llvm")
+if (NOT EXISTS "${LLVM_SRC_ROOT}/build")
+    message(FATAL_ERROR "Cannot find LLVM dir: ${LLVM_SRC_ROOT}/build")
+endif ()
+set (LLVM_DIR ${LLVM_SRC_ROOT}/build/lib/cmake/llvm)
 
+find_package(LLVM REQUIRED CONFIG)
+include_directories(${LLVM_INCLUDE_DIRS})
+add_definitions(${LLVM_DEFINITIONS})
+message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
+
+message(STATUS "unit_common.cmake included")
