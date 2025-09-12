91e97f2e4f4a4586299fa1bfcb5d270f96ed0c9d
liang.he@intel.com <liang.he@intel.com>
2025-09-12 02:12:14 +0000

    fix: declare loop variable in for loop for clarity

--- build-scripts/warnings.cmake
+++ build-scripts/warnings.cmake
@@ -22,14 +22,22 @@ else ()
   add_compile_options (
     $<$<COMPILE_LANGUAGE:C>:-Wimplicit-function-declaration>
   )
+
   # https://gcc.gnu.org/gcc-5/changes.html introduces incompatible-pointer-types
-  if (CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL "5.1")
+  # https://releases.llvm.org/7.0.0/tools/clang/docs/DiagnosticsReference.html#wincompatible-pointer-types
+  #   is the earliest version that supports this option I can found.
+  # Assume AppClang versioning is compatible with Clang.
+  if ((CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL "5.1")
+      OR (CMAKE_C_COMPILER_ID STREQUAL "Clang" AND CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL "7.0.0")
+      OR (CMAKE_C_COMPILER_ID STREQUAL "AppClang" AND CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL "7.0.0"))
     add_compile_options($<$<COMPILE_LANGUAGE:C>:-Wincompatible-pointer-types>)
   endif()
+
   # options benefit embedded system.
   add_compile_options (
     -Wdouble-promotion
   )
+
   # waivers
   add_compile_options (
     -Wno-unused
