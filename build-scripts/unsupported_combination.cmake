081c3445ef2b04b9807763dfdf2273613fb7c925
liang.he <liang.he@intel.com>
2025-12-01 17:55:07 +0800

    feat: add support for checking unsupported build configurations (#4690)
    * feat: add support for checking unsupported build configurations
    * docs: improve clarity and formatting in build_wamr.md
    * feat: add check for unsupported SHARED_HEAP + FAST_JIT configuration and update documentation warning
    * feat: disable default SIMD setting for 64-bit platform in CMake configuration
    * feat: update Android and macOS workflows to handle SIMD support in classic interp mode
    * feat: update SGX compilation workflow to handle unsupported FAST_JIT and classic interp mode without SIMD support
    * feat: enhance check for unsupported CLASSIC_INTERP configurations
    * feat: disable fast interpreter for shared heap tests and update unsupported features test library
    * feat: enhance unsupported combination checks and update build configurations for JIT and SIMD
    * In regression tests, use llvm-jit and fast-jit to replace multi-tier-jit + running mode. Multi-tier-jit contains both fast-jit and llvm-jit. Fast-jit doesn't support SIMD, but llvm-jit does. So, should multi-tier-jit support SIMD? My answer is NO.
      - The regular form of multi-tier-jit uses fast-jit as tier1 and llvm-jit as
        tier2. Therefore, if fast-jit doesn't support SIMD, the entire multi-tier
        doesn't support SIMD either.
      - `--fast-jit` and `--llvm-jit` of multi-tier-jit  should adhere to the global
        limitations of multi-tier-jit.
    * Explicitly specify SIMD, MULTI_MODULE, and their unsupported running modes.
    - SIMD and Ref. types are enabled by default on linux and darwin. So remove duplicate feature testing from CI
    - Apply new configuration for wasm-c-api compilation commands

---
+++ build-scripts/unsupported_combination.cmake
@@ -0,0 +1,102 @@
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+include(CMakePrintHelpers)
+
+# Define a function to check for unsupported combinations
+function(check_aot_mode_error error_message)
+  if(WAMR_BUILD_AOT EQUAL 1)
+    message(FATAL_ERROR "${error_message}")
+  endif()
+endfunction()
+
+# Define a function to check for unsupported combinations with CLASSIC_INTERP
+function(check_classic_interp_error error_message)
+  # Usually, Enable INTERP to enable wasm loader for JIT
+  # WAMR_BUILD_JIT might be undefined, so check it first
+  if(WAMR_BUILD_JIT EQUAL 1)
+    return()
+  endif()
+
+  if(WAMR_BUILD_FAST_JIT EQUAL 1)
+    return()
+  endif()
+
+  if(WAMR_BUILD_INTERP EQUAL 1 AND WAMR_BUILD_FAST_INTERP EQUAL 0)
+    message(FATAL_ERROR "${error_message}")
+  endif()
+endfunction()
+
+# Define a function to check for unsupported combinations with FAST_INTERP
+function(check_fast_interp_error error_message)
+  # Usually, Enable INTERP to enable wasm loader for JIT
+  # WAMR_BUILD_JIT might be undefined, so check it first
+  if(WAMR_BUILD_JIT EQUAL 1)
+    return()
+  endif()
+
+  if(WAMR_BUILD_FAST_JIT EQUAL 1)
+    return()
+  endif()
+
+  if(WAMR_BUILD_INTERP EQUAL 1 AND WAMR_BUILD_FAST_INTERP EQUAL 1)
+    message(FATAL_ERROR "${error_message}")
+  endif()
+endfunction()
+
+# Define a function to check for unsupported combinations with FAST_JIT
+function(check_fast_jit_error error_message)
+  if(WAMR_BUILD_FAST_JIT EQUAL 1)
+    message(FATAL_ERROR "${error_message}")
+  endif()
+endfunction()
+
+# Define a function to check for unsupported combinations with LLVM_JIT
+function(check_llvm_jit_error error_message)
+  if(WAMR_BUILD_JIT EQUAL 1)
+    message(FATAL_ERROR "${error_message}")
+  endif()
+endfunction()
+
+# Below are the unsupported combinations checks
+# Please keep this list in sync with tests/unit/unsupported-features/CMakeLists.txt
+# and tests/wamr-test-suites/test_wamr.sh
+#cmake_print_variables(WAMR_BUILD_INTERP WAMR_BUILD_FAST_INTERP WAMR_BUILD_JIT)
+
+if(WAMR_BUILD_EXCE_HANDLING EQUAL 1)
+  check_aot_mode_error("Unsupported build configuration: EXCE_HANDLING + AOT")
+  check_fast_interp_error("Unsupported build configuration: EXCE_HANDLING + FAST_INTERP")
+  check_fast_jit_error("Unsupported build configuration: EXCE_HANDLING + FAST_JIT")
+  check_llvm_jit_error("Unsupported build configuration: EXCE_HANDLING + JIT")
+endif()
+
+if(WAMR_BUILD_GC EQUAL 1)
+  check_fast_jit_error("Unsupported build configuration: GC + FAST_JIT")
+endif()
+
+if(WAMR_BUILD_MEMORY64 EQUAL 1)
+  check_fast_interp_error("Unsupported build configuration: MEMORY64 + FAST_INTERP")
+  check_fast_jit_error("Unsupported build configuration: MEMORY64 + FAST_JIT")
+  check_llvm_jit_error("Unsupported build configuration: MEMORY64 + JIT")
+endif()
+
+if(WAMR_BUILD_MULTI_MEMORY EQUAL 1)
+  check_aot_mode_error("Unsupported build configuration: EXCE_HANDLING + AOT")
+  check_fast_interp_error("Unsupported build configuration: EXCE_HANDLING + FAST_INTERP")
+  check_fast_jit_error("Unsupported build configuration: EXCE_HANDLING + FAST_JIT")
+  check_llvm_jit_error("Unsupported build configuration: EXCE_HANDLING + JIT")
+endif()
+
+if(WAMR_BUILD_MULTI_MODULE EQUAL 1)
+  check_fast_jit_error("Unsupported build configuration: MULTI_MODULE + FAST_JIT")
+  check_llvm_jit_error("Unsupported build configuration: MULTI_MODULE + JIT")
+endif()
+
+if(WAMR_BUILD_SHARED_HEAP EQUAL 1)
+  check_fast_jit_error("Unsupported build configuration: SHARED_HEAP + FAST_JIT")
+endif()
+
+if(WAMR_BUILD_SIMD EQUAL 1)
+  check_classic_interp_error("Unsupported build configuration: SIMD + CLASSIC_INTERP")
+  check_fast_jit_error("Unsupported build configuration: SIMD + FAST_JIT")
+endif()
