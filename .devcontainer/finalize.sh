2689f80c703ae2cad4a7a74f6254b6b6463ea219
liang.he@intel.com <liang.he@intel.com>
2026-01-21 12:51:01 +0800

    [devcontainer] install ocmal stuff
    For gc proposal spec test

--- .devcontainer/finalize.sh
+++ .devcontainer/finalize.sh
@@ -3,10 +3,16 @@ echo "Running finalize script..."
 #
 # Python Package Installation
 #
+echo "--- Installing Python Dependencies\n"
 
 # Upgrade pip first
 python3 -m pip install --no-cache-dir --break-system-packages --upgrade pip
 # Install required packages
 pip3 install --no-cache-dir --break-system-packages -r .devcontainer/requirements.txt
+
+echo "--- Installing Ocaml stuff\n"
+opam init --yes --shell-setup
+eval $(opam env --switch=default)
+opam install --yes dune menhir
 
 echo "Finalize script completed. ✅"
