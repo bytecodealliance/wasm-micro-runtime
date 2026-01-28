733ac3f19dee0db4131ad1b5c60b3a4a7392fe28
TianlongLiang <111852609+TianlongLiang@users.noreply.github.com>
2026-01-28 15:55:19 +0800

    Collective fix (#4808)
    * fix a bug in zephyr platform file api
    * fix a bug in bh queue
    * fix a bug in shared heap malloc when it's memory64

--- core/shared/utils/bh_queue.c
+++ core/shared/utils/bh_queue.c
@@ -96,13 +96,14 @@ bh_queue_destroy(bh_queue *queue)
 bool
 bh_post_msg2(bh_queue *queue, bh_queue_node *msg)
 {
+    bh_queue_mutex_lock(&queue->queue_lock);
+
     if (queue->cnt >= queue->max) {
         queue->drops++;
         bh_free_msg(msg);
+        bh_queue_mutex_unlock(&queue->queue_lock);
         return false;
     }
-
-    bh_queue_mutex_lock(&queue->queue_lock);
 
     if (queue->cnt == 0) {
         bh_assert(queue->head == NULL);
@@ -131,7 +132,9 @@ bh_post_msg(bh_queue *queue, unsigned short tag, void *body, unsigned int len)
 {
     bh_queue_node *msg = bh_new_msg(tag, body, len, NULL);
     if (msg == NULL) {
+        bh_queue_mutex_lock(&queue->queue_lock);
         queue->drops++;
+        bh_queue_mutex_unlock(&queue->queue_lock);
         if (len != 0 && body)
             BH_FREE(body);
         return false;
